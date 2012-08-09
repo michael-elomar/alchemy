a284d1bdbd2a7b40c2969630dcb6f43138dec6de
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-08-09 15:57:12 +0200

    [DEV] : Do not hardcode config dir.

--- config.mk
+++ config.mk
@@ -11,7 +11,7 @@ CONF := KCONFIG_NOTIMESTAMP=1 $(call fullpath,$(BUILD_SYSTEM)/conf)
 QCONF := KCONFIG_NOTIMESTAMP=1 $(call fullpath,$(BUILD_SYSTEM)/qconf)
 
 # Directory where original configurations are stored
-CONFIG_ORIG_DIR := $(TOP_DIR)/config-ymm
+CONFIG_ORIG_DIR := $(TARGET_CONFIG_DIR)
 
 # File where global configuration is stored
 CONFIG_GLOBAL_FILE := $(CONFIG_ORIG_DIR)/global.config
