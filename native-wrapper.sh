0a16ee2971934eb50ab93cb663f9968b5520e90a
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-10-19 11:46:15 +0200

    [DEV] Allow native wrapper to be executed or sourced.

--- native-wrapper.sh
+++ native-wrapper.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script assume it is copied in the staging or final dir
 # folders bin, usr/bin, lib, /usr/lib are subdirectories there
 
-# Full path to this script
-readonly SCRIPT_PATH=`(cd $(dirname $0) && pwd)`
+# Get full path to this script (either when executed or sourced)
+SCRIPT_PATH=`(cd $(dirname ${BASH_SOURCE}) && pwd)`
+
+# Restore previous variables
+if [ "${OLD_PATH}" != "" ]; then
+	export PATH=${OLD_PATH}
+fi
+if [ "${OLD_LD_LIBRARY_PATH}" != "" ]; then
+	export LD_LIBRARY_PATH=${OLD_LD_LIBRARY_PATH}
+fi
+
+# Save previous variables
+OLD_PATH=${PATH}
+OLD_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 
 # Update path
 export PATH=${SCRIPT_PATH}/bin:${SCRIPT_PATH}/usr/bin:${PATH}
@@ -12,8 +24,8 @@ export PATH=${SCRIPT_PATH}/bin:${SCRIPT_PATH}/usr/bin:${PATH}
 # Update library path
 export LD_LIBRARY_PATH=${SCRIPT_PATH}/lib:${SCRIPT_PATH}/usr/lib:${LD_LIBRARY_PATH}
 
-# execute given command line
-if [ "$#" != "0" ]; then
-	$*
+# execute given command line (only of not sourced)
+if [ "${BASH_SOURCE}" = "$0" ]; then
+	$@
 fi
 
