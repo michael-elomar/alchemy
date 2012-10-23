115815c35bd7fd713865714d0a70582bf45cc35c
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-10-23 14:28:53 +0200

    [DEV] Add --enable-silent-rules in configure options.

--- autotools-setup.mk
+++ autotools-setup.mk
@@ -62,7 +62,7 @@ AUTOTOOLS_MAKE_ARGS :=
 
 # Quiet flags
 ifeq ("$(V)","0")
-  AUTOTOOLS_CONFIGURE_ARGS += --quiet
+  AUTOTOOLS_CONFIGURE_ARGS += --quiet --enable-silent-rules
   AUTOTOOLS_MAKE_ENV += LIBTOOLFLAGS="--quiet"
   AUTOTOOLS_MAKE_ARGS += -s --no-print-directory
 endif
