c30c58d7a43f0cbad757986e414b6c527ad9616c
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-10-29 10:39:10 +0100

    [DEV] Code cleaning.

--- make-final.py
+++ make-final.py
@@ -10,7 +10,6 @@
 import sys, os, logging
 import subprocess
 import optparse
-import shutil
 
 #===============================================================================
 # Global variables.
@@ -64,6 +63,77 @@ def canStrip(filePath):
 	return result
 
 #===============================================================================
+#===============================================================================
+def doCopyByMakefile(dstFileName, srcFileName, doStrip, options):
+	# if source file contains ':' or '=' it doesn't work great
+	# prerequisite shall be escaped
+	# target can not contain at all any ':' or '=' so replace with '_'
+	# in command do not use target name as it will be wrong
+	# consequence is that target will never actually exists and commands
+	# will always be executed
+	if srcFileName.find(":") >= 0:
+		srcFileName = srcFileName.replace(":", "\\:")
+		srcFileName = srcFileName.replace("=", "\\=")
+		dstFileName = dstFileName.replace(":", "_")
+		dstFileName = dstFileName.replace("=", "_")
+
+	# register destination in ALL variable
+	options.makefile.write("ALL += %s\n" % dstFileName)
+
+	# rule
+	options.makefile.write("%s: %s\n" % (dstFileName, srcFileName))
+
+	# define variables with real src and dst (in case it was patched above)
+	options.makefile.write("\t$(eval __src := $<)\n")
+	options.makefile.write("\t$(eval __dst := $(patsubst %s/%%,%s/%%,$<))\n" % \
+		(options.stagingDir, options.finalDir))
+
+	# commands
+	options.makefile.write("\t@mkdir -p $(dir $(__dst))\n")
+	options.makefile.write("\t@echo Alchemy install: $(patsubst $(PWD)/%,%,$(__dst))\n")
+	if doStrip:
+		options.makefile.write("\t$(Q)$(STRIP) -o $(__dst) $(__src)\n")
+	else:
+		options.makefile.write("\t$(Q)cp -af $(__src) $(__dst)\n")
+	options.makefile.write("\n")
+
+#===============================================================================
+#===============================================================================
+def doCopy(dstFileName, srcFileName, doStrip, options):
+	# copy and strip executables
+	if doStrip:
+		os.system("%s -o %s %s" % (options.strip, dstFileName, srcFileName))
+	else:
+		os.system("cp -af %s %s" % (srcFileName, dstFileName))
+
+#===============================================================================
+# Makefile banner
+# The .SUFFIXES is important to remove all implicit rules.
+# There is one which is really, really nasty :
+#   a file x is updated from a file x.sh automatically
+# Guess what happen when you have both in a folder :
+#  the executable is replaced by the script if older...
+#===============================================================================
+def writeMakefileHeader(options):
+	options.makefile.write("# GENERATED FILE, DO NOT MODIFY\n\n")
+	options.makefile.write(".SUFFIXES:\n\n")
+	if options.strip != None:
+		options.makefile.write("STRIP := %s\n" % options.strip)
+	options.makefile.write("PWD := $(shell pwd)\n")
+	options.makefile.write("ALL :=\n")
+	options.makefile.write("ifneq (\"$(V)\",\"1\")\n")
+	options.makefile.write("  Q := @\n")
+	options.makefile.write("endif\n")
+	options.makefile.write(".PHONY: all\n")
+	options.makefile.write("all: do-all\n\n")
+
+#===============================================================================
+#===============================================================================
+def writeMakefileFooter(options):
+	options.makefile.write(".PHONY: do-all\n")
+	options.makefile.write("do-all: $(ALL)\n\n")
+
+#===============================================================================
 # Main function.
 #===============================================================================
 def main():
@@ -71,58 +141,42 @@ def main():
 	setupLog(options)
 
 	# get parameters
-	stagingDir = args[0]
-	finalDir = args[1]
-	logging.info("staging-dir : %s", stagingDir)
-	logging.info("final-dir : %s", finalDir)
+	options.stagingDir = args[0]
+	options.finalDir = args[1]
+	logging.info("staging-dir : %s", options.stagingDir)
+	logging.info("final-dir : %s", options.finalDir)
 
 	# do we need to output a makefile ?
-	makefile = None
+	options.makefile = None
 	if len(args) >= 3:
 		logging.info("makefile : %s", args[2])
-		makefile = open(args[2], "w")
+		options.makefile = open(args[2], "w")
 
 	# check that staging directory exists
-	if not os.path.isdir(stagingDir):
-		logging.error("%s is not a directory", stagingDir)
+	if not os.path.isdir(options.stagingDir):
+		logging.error("%s is not a directory", options.stagingDir)
 
-	# Makefile banner
-	# The .SUFFIXES is important to remove all impicit rules.
-	# There is one which is really, really nasty :
-	#   a file x is updated from a file x.sh automatically
-	# Guess what happen when you have both in a folder :
-	#  the executable is replace by the script if older...
-	if makefile != None:
-		makefile.write("# GENERATED FILE, DO NOT MODIFY\n\n")
-		makefile.write(".SUFFIXES:\n\n")
-		if options.strip != None:
-			makefile.write("STRIP := %s\n" % options.strip)
-		makefile.write("PWD := $(shell pwd)\n")
-		makefile.write("ALL :=\n")
-		makefile.write("ifneq (\"$(V)\",\"1\")\n")
-		makefile.write("  Q := @\n")
-		makefile.write("endif\n")
-		makefile.write(".PHONY: all\n")
-		makefile.write("all: do-all\n\n")
+	if options.makefile != None:
+		writeMakefileHeader(options)
 
 	# browse staging directory
-	for (dirPath, dirNames, fileNames) in os.walk(stagingDir):
+	for (dirPath, dirNames, fileNames) in os.walk(options.stagingDir):
 		# exclude some directories
 		for dirName in EXCLUDE_DIRS:
 			if dirName in dirNames:
 				logging.debug("Exclude directory : %s",
-					os.path.relpath(os.path.join(dirPath, dirName), stagingDir))
+					os.path.relpath(os.path.join(dirPath, dirName), options.stagingDir))
 				dirNames.remove(dirName)
 		for fileName in fileNames:
-			# skip dome extensions
+			# skip some extensions
 			srcFileName = os.path.join(dirPath, fileName)
-			relPath = os.path.relpath(srcFileName, stagingDir)
+			relPath = os.path.relpath(srcFileName, options.stagingDir)
 			if os.path.splitext(srcFileName)[1] in EXCLUDE_FILTERS:
 				logging.debug("Exclude file : %s", relPath) 
 				continue
 			logging.info("File : %s", relPath)
 			# destination
-			dstFileName = os.path.join(finalDir, relPath)
+			dstFileName = os.path.join(options.finalDir, relPath)
 			dstDirName = os.path.split(dstFileName)[0]
 			if not os.path.exists(dstDirName):
 				os.makedirs(dstDirName, 0755)
@@ -154,50 +208,13 @@ def main():
 				continue
 
 			# go
-			if makefile != None:
-				# if source file contains ':' or '=' it doesn't work great
-				# prerequisite shall be escaped
-				# target can not contain at all  any ':' or '=' so replace with '_'
-				# in command do not use target name as it will be wrong
-				# consequence is that target will never actually exists and commands
-				# will always be executed
-				patched = True
-				if srcFileName.find(":") >= 0:
-					srcFileName = srcFileName.replace(":", "\\:")
-					srcFileName = srcFileName.replace("=", "\\=")
-					dstFileName = dstFileName.replace(":", "_")
-					dstFileName = dstFileName.replace("=", "_")
-
-				# register destination in ALL variable
-				makefile.write("ALL += %s\n" % dstFileName)
-
-				# rule
-				makefile.write("%s: %s\n" % (dstFileName, srcFileName))
-
-				# define variables with real src and dst (in case it was patched above)
-				makefile.write("\t$(eval __src := $<)\n")
-				makefile.write("\t$(eval __dst := $(patsubst %s/%%,%s/%%,$<))\n" % \
-					(stagingDir, finalDir))
-
-				# commands
-				makefile.write("\t@mkdir -p $(dir $(__dst))\n")
-				makefile.write("\t@echo Alchemy install: $(patsubst $(PWD)/%,%,$(__dst))\n")
-				if doStrip:
-					makefile.write("\t$(Q)$(STRIP) -o $(__dst) $(__src)\n")
-				else:
-					makefile.write("\t$(Q)cp -af $(__src) $(__dst)\n")
-				makefile.write("\n")
+			if options.makefile != None:
+				doCopyByMakefile(dstFileName, srcFileName, doStrip, options)
 			else:
-				# copy and strip executables
-				if doAction:
-					if doStrip:
-						os.system("%s -o %s %s" % (options.strip, dstFileName, srcFileName))
-					else:
-						shutil.copy2(srcFileName, dstFileName)
+				doCopy(dstFileName, srcFileName, doStrip, options)
 
-	if makefile != None:
-		makefile.write(".PHONY: do-all\n")
-		makefile.write("do-all: $(ALL)\n\n")
+	if options.makefile != None:
+		writeMakefileFooter(options)
 
 #===============================================================================
 # Setup option parser and parse command line.
