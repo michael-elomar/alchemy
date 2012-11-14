b93faac03baf9d7e07db71275f9b32693f3cb971
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-11-14 14:08:22 +0100

    [DEV] Update ecos makefile.
    - Add support for clean and config targets.

--- test/patch/Libraries#SoftParrot5#ecos#ecos-current#atom.mk
+++ test/patch/Libraries#SoftParrot5#ecos#ecos-current#atom.mk
@@ -8,8 +8,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ecos
 LOCAL_MODULE_FILENAME := $(LOCAL_MODULE).done
 
-LOCAL_DONE_FILES := $(LOCAL_MODULE).done $(LOCAL_MODULE)-installed.done
-
 ECOS_CONFIG_FILE := $(TARGET_CONFIG_DIR)/ecos.ecm
 
 ECOS_DIR := $(LOCAL_PATH)
@@ -17,6 +15,7 @@ ECOS_BUILD_DIR := $(call local-get-build-dir)
 ECOS_DONE_FILE := $(ECOS_BUILD_DIR)/$(LOCAL_MODULE_FILENAME)
 
 ECOS_CONFIG_BIN := $(ECOS_DIR)/tools/bin-linux/ecosconfig
+ECOS_CONFIGTOOL_BIN := $(ECOS_DIR)/tools/bin-linux/configtool2
 ECOS_REPOSITORY := $(ECOS_DIR)/packages
 
 ECOS_MAKEFILE := $(ECOS_BUILD_DIR)/makefile
@@ -38,8 +37,15 @@ ECOS_CONFIG= \
 	--prefix=$(ECOS_INSTALL_DIR) \
 	--srcdir=$(ECOS_REPOSITORY)
 
+ifeq ("$(V)","0")
+  ECOS_MAKE_ARGS := -s
+else
+  ECOS_MAKE_ARGS :=
+endif
+
 # Generate ecos makefile
-$(ECOS_MAKEFILE): $(ECOS_CONFIG_FILE)
+$(ECOS_ECC_FILE) $(ECOS_MAKEFILE): $(ECOS_CONFIG_FILE)
+	@echo "Importing ecos configuration: $(ECOS_CONFIG_FILE)"
 	@mkdir -p $(ECOS_BUILD_DIR)
 	@mkdir -p $(ECOS_INSTALL_DIR)
 	@cat /dev/null > $(ECOS_ECC_FILE)
@@ -48,30 +54,31 @@ $(ECOS_MAKEFILE): $(ECOS_CONFIG_FILE)
 	$(Q)cd $(ECOS_BUILD_DIR) && $(ECOS_CONFIG) resolve
 	$(Q)cd $(ECOS_BUILD_DIR) && $(ECOS_CONFIG) tree
 
-ifeq ("$(V)","0")
-  ECOS_MAKE_ARGS := -s
-else
-  ECOS_MAKE_ARGS :=
-endif
-
-# Build ecos
-.PHONY: ecos-build
-ecos-build: $(ECOS_MAKEFILE)
-	@echo "Building ecos"
-	$(Q)cd $(ECOS_BUILD_DIR) && $(MAKE) $(ECOS_MAKE_ARGS)
-	@echo "Building ecos: done"
-
 # Module main rule
-$(ECOS_DONE_FILE): | ecos-build
+$(ECOS_DONE_FILE): $(ECOS_MAKEFILE)
+	@echo "Building ecos"
+	$(Q)$(MAKE) $(ECOS_MAKE_ARGS) -C $(ECOS_BUILD_DIR)
+	@echo "Building ecos: done"
 	@touch $@
 
 # As a special exception, this variable is modified to make sure ecos is build
 # before anything happens
 TARGET_GLOBAL_PREREQUISITES += $(ECOS_DONE_FILE)
 
-# Additional clean variables
-LOCAL_CLEAN_DIRS +=$(ECOS_BUILD_DIR) $(ECOS_INSTALL_DIR)
-LOCAL_CLEAN_FILES += $(ECOS_ECC_FILE)
+# Custom clean rule. LOCAL_MODULE_FILENAME already deleted by common rule
+# make clean may fail, so ignore its error
+.PHONY: ecos-clean
+ecos-clean:
+	$(Q)if [ -d $(ECOS_BUILD_DIR) ]; then \
+		$(MAKE) $(ECOS_MAKE_ARGS) -C $(ECOS_BUILD_DIR) clean; \
+	fi
+
+# Ecos configuration
+.PHONY: ecos-config
+ecos-config: $(ECOS_ECC_FILE)
+	@echo "Configuring ecos: $(ECOS_CONFIG_FILE)"
+	$(Q)cd $(ECOS_BUILD_DIR) && $(ECOS_CONFIGTOOL_BIN) \
+		$(ECOS_REPOSITORY) $(ECOS_ECC_FILE)
 
 $(call local-add-module)
 
