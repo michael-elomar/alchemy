aabc7abace75d5a052d0cdfac79a340e769c41c4
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-11-19 14:16:47 +0100

    [DEV] Use skeleton.

--- test/patch/Alchemy-build-pclinux-chroot.sh
+++ test/patch/Alchemy-build-pclinux-chroot.sh
@@ -15,6 +15,7 @@ export TARGET_OUT_STAGING="${TARGET_OUT}/staging"
 export TARGET_OUT_FINAL="${TARGET_OUT}/final"
 export TARGET_CONFIG_DIR="${TOP_DIR}/Alchemy/test/config/${TARGET_PRODUCT}-${TARGET_PRODUCT_VARIANT}"
 export TARGET_SCAN_PRUNE_DIRS="Alchemy-out raptor"
+export TARGET_SKEL="${TOP_DIR}/Alchemy/test/native-chroot-skel"
 
 export USE_COLORS=1
 
