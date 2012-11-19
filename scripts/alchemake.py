b1d5ad374b92700dae63ef64ed99392f6a062ed9
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-11-19 11:12:43 +0100

    [DEBUG] Make it work with menuconfig.
    - do not redirect stdout
    - make the spanwed process leader of its process group

--- scripts/alchemake.py
+++ scripts/alchemake.py
@@ -1,77 +1,69 @@
 #!/usr/bin/env python
 
 import sys, os
-import subprocess, threading
+import subprocess
 import signal
 import re
-
-shouldStop = False
-
-#===============================================================================
-#===============================================================================
-def readerThread(p, fh, n):
-	global shouldStop
-	# Regex for error line to detect
-	reError = re.compile(r"make(\[[0-9]+\])?: \*\*\* \[[^\[\]]*\] Error [0-9]+")
-	while not shouldStop:
-		# Empty line means EOF, so exit
-		line = fh.readline()
-		if len(line) == 0:
-			return
-		line = line.rstrip("\n")
-
-		# Display line in the correct stream
-		if n == 1:
-			sys.stdout.write(line+"\n")
-		else:
-			sys.stderr.write(line+"\n")
-
-		# Detect error in stderr
-		if not shouldStop and n == 2 and reError.match(line):
-			sys.stderr.write("\n\033[31mMAKE ERROR DETECTED\n\033[00m")
-			# Kill the process group (so all sub-makes...)
-			shouldStop = True
-			p.terminate()
-
-#===============================================================================
-#===============================================================================
-def signalHandler(sig, frame):
-	global shouldStop
-	shouldStop = True
-	sys.exit(1)
 
 #===============================================================================
 # Main function.
 #===============================================================================
 def main():
+	shouldStop = False
+	p = None
+
+	# Signal handler, notify main loop to exit and kill subprocess
+	# SIGKILL is to kill whatever process that did not stop after SIGTERM
+	def signalHandler(sig, frame):
+		shouldStop = True
+		if p != None:
+			os.killpg(p.pid, signal.SIGTERM)
+			os.killpg(p.pid, signal.SIGKILL)
+		else:
+			sys.exit(1)
+
 	# Try to exit silently in case of interrupts...
 	signal.signal(signal.SIGINT, signalHandler)
 	signal.signal(signal.SIGTERM, signalHandler)
 
-	# call make with given arguments, stat in a shell so we can kill it
-	# and all its child automatically
-	cmdArgs = "make"
-	for arg in sys.argv[1:]:
-		cmdArgs += " \"" + arg + "\""
+	# Make the new child leader of a new process group so we can kill all
+	# its children at once
+	def preExec():
+		signal.signal(signal.SIGTTOU, signal.SIG_IGN)
+		os.setpgid(0, 0)
+		os.tcsetpgrp(0, os.getpid())
+
+	# Only redirect stderr (redirecting stdout causes issues if a child process
+	# wants to use the terminal like ncurses)
+	cmdArgs = ["make"] + sys.argv[1:]
 	p = subprocess.Popen(cmdArgs,
-		stdout=subprocess.PIPE,
 		stderr=subprocess.PIPE,
-		shell=True)
+		preexec_fn=preExec,
+		shell=False)
 
-	# Start thread to read stdout/stderr
-	stdoutThread = threading.Thread(target=readerThread, args=(p, p.stdout, 1))
-	stdoutThread.setDaemon(True)
-	stdoutThread.start()
-	stderrThread = threading.Thread(target=readerThread, args=(p, p.stderr, 2))
-	stderrThread.setDaemon(True)
-	stderrThread.start()
+	# Read from stderr redirected in a pipe
+	reError = re.compile(r"make(\[[0-9]+\])?: \*\*\* \[[^\[\]]*\] Error [0-9]+")
+	while not shouldStop:
+		try:
+			# Empty line means EOF detected, so exit loop
+			line = p.stderr.readline()
+			if len(line) == 0:
+				break
+			sys.stderr.write(line)
+			# Check for sub-make error and kill the process group
+			# SIGKILL is to kill whatever process that did not stop after SIGTERM
+			if reError.match(line):
+				sys.stderr.write("\n\033[31mMAKE ERROR DETECTED\n\033[00m")
+				os.killpg(p.pid, signal.SIGTERM)
+				os.killpg(p.pid, signal.SIGKILL)
+				break
+		except IOError as ex:
+			# Will occur when interrupted during read, shouldStop will be set
+			# at True in signalHandler
+			pass
 
-	# Wait everybody
-	stdoutThread.join()
-	stderrThread.join()
+	# Exit with same result as sub-process
 	p.wait()
-
-	# Exit with same result as make
 	sys.exit(p.returncode)
 
 #===============================================================================
