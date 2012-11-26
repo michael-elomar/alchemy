74f976b45cb5599f4e26219ad21695c91c4c969d
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-11-26 09:33:50 +0100

    [DEBUG] Handle french locale in regex, add delay in kill.
    - Do not use or kill process group when called in a sub-make.
    - The assignation at True shouldStop was in fact done in a new
      variable. So remove the feature completely.

--- scripts/alchemake.py
+++ scripts/alchemake.py
@@ -3,22 +3,29 @@
 import sys, os
 import subprocess
 import signal
+import time
 import re
 
 #===============================================================================
 # Main function.
 #===============================================================================
 def main():
-	shouldStop = False
 	p = None
 
-	# Signal handler, notify main loop to exit and kill subprocess
+	# If MAKELEVEL is defined, we are in a sub-make so don't play with
+	# process groups or killing
+	useKillpg = (not "MAKELEVEL" in os.environ)
+
+	# Signal handler, kill subprocess
 	# SIGKILL is to kill whatever process that did not stop after SIGTERM
 	def signalHandler(sig, frame):
-		shouldStop = True
 		if p != None:
-			os.killpg(p.pid, signal.SIGTERM)
-			os.killpg(p.pid, signal.SIGKILL)
+			if useKillpg:
+				os.killpg(p.pid, signal.SIGTERM)
+				time.sleep(1)
+				os.killpg(p.pid, signal.SIGKILL)
+			else:
+				p.terminate()
 		else:
 			sys.exit(1)
 
@@ -29,12 +36,13 @@ def main():
 	# Make the new child leader of a new process group so we can kill all
 	# its children at once
 	def preExec():
-		signal.signal(signal.SIGTTOU, signal.SIG_IGN)
-		os.setpgid(0, 0)
-		os.tcsetpgrp(0, os.getpid())
+		if useKillpg:
+			signal.signal(signal.SIGTTOU, signal.SIG_IGN)
+			os.setpgid(0, 0)
+			os.tcsetpgrp(0, os.getpid())
 
 	# Only redirect stderr (redirecting stdout causes issues if a child process
-	# wants to use the terminal like ncurses)
+	# wants to use the terminal, like ncurses)
 	cmdArgs = ["make"] + sys.argv[1:]
 	p = subprocess.Popen(cmdArgs,
 		stderr=subprocess.PIPE,
@@ -42,8 +50,8 @@ def main():
 		shell=False)
 
 	# Read from stderr redirected in a pipe
-	reError = re.compile(r"make(\[[0-9]+\])?: \*\*\* \[[^\[\]]*\] Error [0-9]+")
-	while not shouldStop:
+	reError = re.compile(r"make(\[[0-9]+\])?: \*\*\* \[[^\[\]]*\] (Error|Erreur) [0-9]+")
+	while True:
 		try:
 			# Empty line means EOF detected, so exit loop
 			line = p.stderr.readline()
@@ -54,12 +62,15 @@ def main():
 			# SIGKILL is to kill whatever process that did not stop after SIGTERM
 			if reError.match(line):
 				sys.stderr.write("\n\033[31mMAKE ERROR DETECTED\n\033[00m")
-				os.killpg(p.pid, signal.SIGTERM)
-				os.killpg(p.pid, signal.SIGKILL)
+				if useKillpg:
+					os.killpg(p.pid, signal.SIGTERM)
+					time.sleep(1)
+					os.killpg(p.pid, signal.SIGKILL)
+				else:
+					p.terminate()
 				break
 		except IOError as ex:
-			# Will occur when interrupted during read, shouldStop will be set
-			# at True in signalHandler
+			# Will occur when interrupted during read, an EOF will be read next
 			pass
 
 	# Exit with same result as sub-process
