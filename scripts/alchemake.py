f80e23263d7c463437df9c57c08e0db492a06b5e
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-11-29 19:30:30 +0100

    [DEBUG] Better handling of interruption when error detected.
    - Only interrupt after the top level make has printed its error so we
      can have a 'backtrace'.
    - Try to interrupt first then kill remaining processes in the group.
    - Stop printing errors when an error is detected (avoid message of
      interrupted make).

--- scripts/alchemake.py
+++ scripts/alchemake.py
@@ -7,6 +7,19 @@ import time
 import re
 
 #===============================================================================
+# Kill make
+# p: make sub-process.
+#===============================================================================
+def killMake(p):
+	# Interrupt top level make, then everyone in the process group,
+	# then kill remaining Some time later
+	os.kill(p.pid, signal.SIGINT)
+	time.sleep(1)
+	os.killpg(p.pid, signal.SIGINT)
+	time.sleep(1)
+	os.killpg(p.pid, signal.SIGKILL)
+
+#===============================================================================
 # Main function.
 #===============================================================================
 def main():
@@ -21,9 +34,7 @@ def main():
 	def signalHandler(sig, frame):
 		if p != None:
 			if useKillpg:
-				os.killpg(p.pid, signal.SIGTERM)
-				time.sleep(1)
-				os.killpg(p.pid, signal.SIGKILL)
+				killMake(p)
 			else:
 				p.terminate()
 		else:
@@ -50,28 +61,33 @@ def main():
 		shell=False)
 
 	# Read from stderr redirected in a pipe
-	reError = re.compile(r"make(\[[0-9]+\])?: \*\*\* \[[^\[\]]*\] (Error|Erreur) [0-9]+")
+	# only catch top level makefile errors (sub-make files error will eventually
+	# generate a top level error)
+	reError1 = re.compile(r"make: \*\*\* No rule to make target .*")
+	reError2 = re.compile(r"make: \*\*\* \[[^\[\]]*\] (Error|Erreur) [0-9]+")
+	errorDetected = False
 	while True:
 		try:
 			# Empty line means EOF detected, so exit loop
 			line = p.stderr.readline()
 			if len(line) == 0:
 				break
-			sys.stderr.write(line)
-			# Check for sub-make error and kill the process group
-			# SIGKILL is to kill whatever process that did not stop after SIGTERM
-			if reError.match(line):
-				sys.stderr.write("\n\033[31mMAKE ERROR DETECTED\n\033[00m")
+			if not errorDetected:
+				sys.stderr.write(line)
+			# Check for make error
+			if reError1.match(line) or reError2.match(line):
+				errorDetected = True
 				if useKillpg:
-					os.killpg(p.pid, signal.SIGTERM)
-					time.sleep(1)
-					os.killpg(p.pid, signal.SIGKILL)
+					killMake(p)
 				else:
 					p.terminate()
-				break
 		except IOError as ex:
 			# Will occur when interrupted during read, an EOF will be read next
 			pass
+
+	# Only prinf message once at the end
+	if errorDetected:
+		sys.stderr.write("\n\033[31mMAKE ERROR DETECTED\n\033[00m")
 
 	# Exit with same result as sub-process
 	p.wait()
