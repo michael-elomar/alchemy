78c683b8858fc294addd791032eec528a87efda3
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-12-10 10:21:13 +0100

    [DEV] Add BUILD_CUSTOM makefile.

---
+++ custom.mk
@@ -0,0 +1,15 @@
+###############################################################################
+## @file custom.mk
+## @author Y.M. Morgan
+## @date 2012/12/07
+##
+## Handle modules using custom rules.
+###############################################################################
+
+LOCAL_MODULE_CLASS := CUSTOM
+
+ifeq ("$(LOCAL_MODULE_FILENAME)","")
+LOCAL_MODULE_FILENAME := $(LOCAL_MODULE).done
+endif
+
+$(local-add-module)
--- main.mk
+++ main.mk
@@ -153,6 +153,7 @@ BUILD_STATIC_LIBRARY := $(BUILD_SYSTEM)/static.mk
 BUILD_SHARED_LIBRARY := $(BUILD_SYSTEM)/shared.mk
 BUILD_EXECUTABLE := $(BUILD_SYSTEM)/executable.mk
 BUILD_AUTOTOOLS := $(BUILD_SYSTEM)/autotools.mk
+BUILD_CUSTOM := $(BUILD_SYSTEM)/custom.mk
 BUILD_PREBUILT := $(BUILD_SYSTEM)/prebuilt.mk
 
 # Shall be defined before including user makefiles
