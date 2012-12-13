e0eea540760becad9b898e9c1d24c69d4b0206db
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2012-12-13 17:42:10 +0100

    [DEV] Rename fc6050 in fc6050w

--- test/patch/Alchemy-build-fc6050w.sh
+++ test/patch/Alchemy-build-fc6050w.sh
@@ -2,7 +2,7 @@
 
 readonly TOP_DIR=`pwd`
 
-export TARGET_PRODUCT="fc6050"
+export TARGET_PRODUCT="fc6050w"
 export TARGET_PRODUCT_VARIANT="hiphop"
 export TARGET_OS="linux"
 export TARGET_OS_FLAVOUR="parrot"
@@ -13,7 +13,7 @@ export TARGET_OUT="${TOP_DIR}/Alchemy-out/${TARGET_PRODUCT}-${TARGET_PRODUCT_VAR
 export TARGET_OUT_BUILD="${TARGET_OUT}/build"
 export TARGET_OUT_STAGING="${TARGET_OUT}/staging"
 export TARGET_OUT_FINAL="${TARGET_OUT}/final"
-export TARGET_SKEL="${TOP_DIR}/Data/Common"
+#export TARGET_SKEL="${TOP_DIR}/Data/Common"
 export TARGET_CONFIG_DIR="${TOP_DIR}/Alchemy/test/config/${TARGET_PRODUCT}-${TARGET_PRODUCT_VARIANT}"
 export TARGET_SCAN_PRUNE_DIRS="Alchemy-out raptor"
 export TARGET_LINUX_DIR="${TOP_DIR}/Linux/kernel/linux"
