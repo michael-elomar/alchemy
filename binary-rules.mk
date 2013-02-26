7203d72fddb686dacba33f6cf45cc87bf28a9f19
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-02-26 13:38:21 +0100

    [CR 0106621] Add support for identical filenames, apart from extension, in source files
    Use .c.o etc... for object file names.

--- binary-rules.mk
+++ binary-rules.mk
@@ -62,40 +62,40 @@ $(call check-flags,LOCAL_EXPORT_CXXFLAGS,$(check-flags-debug),$(check-flags-debu
 ###############################################################################
 
 cpp_sources := $(filter %.cpp,$(LOCAL_SRC_FILES))
-cpp_objects := $(addprefix $(build_dir)/obj/,$(cpp_sources:.cpp=.o))
+cpp_objects := $(addprefix $(build_dir)/obj/,$(cpp_sources:.cpp=.cpp.o))
 
 cxx_sources := $(filter %.cxx,$(LOCAL_SRC_FILES))
-cxx_objects := $(addprefix $(build_dir)/obj/,$(cxx_sources:.cxx=.o))
+cxx_objects := $(addprefix $(build_dir)/obj/,$(cxx_sources:.cxx=.cxx.o))
 
 cc_sources := $(filter %.cc,$(LOCAL_SRC_FILES))
-cc_objects := $(addprefix $(build_dir)/obj/,$(cc_sources:.cc=.o))
+cc_objects := $(addprefix $(build_dir)/obj/,$(cc_sources:.cc=.cc.o))
 
 c_sources := $(filter %.c,$(LOCAL_SRC_FILES))
-c_objects := $(addprefix $(build_dir)/obj/,$(c_sources:.c=.o))
+c_objects := $(addprefix $(build_dir)/obj/,$(c_sources:.c=.c.o))
 
 s_sources := $(filter %.s,$(LOCAL_SRC_FILES))
-s_objects := $(addprefix $(build_dir)/obj/,$(s_sources:.s=.o))
+s_objects := $(addprefix $(build_dir)/obj/,$(s_sources:.s=.s.o))
 
 S_sources := $(filter %.S,$(LOCAL_SRC_FILES))
-S_objects := $(addprefix $(build_dir)/obj/,$(S_sources:.S=.o))
+S_objects := $(addprefix $(build_dir)/obj/,$(S_sources:.S=.S.o))
 
 gen_cpp_sources := $(filter %.cpp,$(LOCAL_GENERATED_SRC_FILES))
-gen_cpp_objects := $(addprefix $(build_dir)/obj/,$(gen_cpp_sources:.cpp=.o))
+gen_cpp_objects := $(addprefix $(build_dir)/obj/,$(gen_cpp_sources:.cpp=.cpp.o))
 
 gen_cxx_sources := $(filter %.cxx,$(LOCAL_GENERATED_SRC_FILES))
-gen_cxx_objects := $(addprefix $(build_dir)/obj/,$(gen_cxx_sources:.cxx=.o))
+gen_cxx_objects := $(addprefix $(build_dir)/obj/,$(gen_cxx_sources:.cxx=.cxx.o))
 
 gen_cc_sources := $(filter %.cc,$(LOCAL_GENERATED_SRC_FILES))
-gen_cc_objects := $(addprefix $(build_dir)/obj/,$(gen_cc_sources:.cc=.o))
+gen_cc_objects := $(addprefix $(build_dir)/obj/,$(gen_cc_sources:.cc=.cc.o))
 
 gen_c_sources := $(filter %.c,$(LOCAL_GENERATED_SRC_FILES))
-gen_c_objects := $(addprefix $(build_dir)/obj/,$(gen_c_sources:.c=.o))
+gen_c_objects := $(addprefix $(build_dir)/obj/,$(gen_c_sources:.c=.c.o))
 
 gen_s_sources := $(filter %.s,$(LOCAL_GENERATED_SRC_FILES))
-gen_s_objects := $(addprefix $(build_dir)/obj/,$(gen_s_sources:.s=.o))
+gen_s_objects := $(addprefix $(build_dir)/obj/,$(gen_s_sources:.s=.s.o))
 
 gen_S_sources := $(filter %.S,$(LOCAL_GENERATED_SRC_FILES))
-gen_S_objects := $(addprefix $(build_dir)/obj/,$(gen_S_sources:.S=.o))
+gen_S_objects := $(addprefix $(build_dir)/obj/,$(gen_S_sources:.S=.S.o))
 
 all_gen_sources := \
 	$(gen_cpp_sources) \
@@ -243,7 +243,7 @@ endif
 
 # cpp files
 ifneq ("$(strip $(cpp_objects))","")
-$(cpp_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.cpp
+$(cpp_objects): $(build_dir)/obj/%.cpp.o: $(LOCAL_PATH)/%.cpp
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(cpp_objects:%.o=%.d)
@@ -252,7 +252,7 @@ endif
 
 # cxx files
 ifneq ("$(strip $(cxx_objects))","")
-$(cxx_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.cxx
+$(cxx_objects): $(build_dir)/obj/%.cxx.o: $(LOCAL_PATH)/%.cxx
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(cxx_objects:%.o=%.d)
@@ -261,7 +261,7 @@ endif
 
 # cc files
 ifneq ("$(strip $(cc_objects))","")
-$(cc_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.cc
+$(cc_objects): $(build_dir)/obj/%.cc.o: $(LOCAL_PATH)/%.cc
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(cc_objects:%.o=%.d)
@@ -270,7 +270,7 @@ endif
 
 # c files
 ifneq ("$(strip $(c_objects))","")
-$(c_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.c
+$(c_objects): $(build_dir)/obj/%.c.o: $(LOCAL_PATH)/%.c
 	$(transform-c-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(c_objects:%.o=%.d)
@@ -280,14 +280,14 @@ endif
 # s files
 # There is NO dependency files for raw asm code...
 ifneq ("$(strip $(s_objects))","")
-$(s_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.s
+$(s_objects): $(build_dir)/obj/%.s.o: $(LOCAL_PATH)/%.s
 	$(transform-s-to-o)
 endif
 
 # S files
 # There is dependency files for asm code...
 ifneq ("$(strip $(S_objects))","")
-$(S_objects): $(build_dir)/obj/%.o: $(LOCAL_PATH)/%.S
+$(S_objects): $(build_dir)/obj/%.S.o: $(LOCAL_PATH)/%.S
 	$(transform-s-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(S_objects:%.o=%.d)
@@ -296,7 +296,7 @@ endif
 
 # Generated cpp files
 ifneq ("$(strip $(gen_cpp_objects))","")
-$(gen_cpp_objects): $(build_dir)/obj/%.o: $(build_dir)/%.cpp
+$(gen_cpp_objects): $(build_dir)/obj/%.cpp.o: $(build_dir)/%.cpp
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(gen_cpp_objects:%.o=%.d)
@@ -305,7 +305,7 @@ endif
 
 # Generated cxx files
 ifneq ("$(strip $(gen_cxx_objects))","")
-$(gen_cxx_objects): $(build_dir)/obj/%.o: $(build_dir)/%.cxx
+$(gen_cxx_objects): $(build_dir)/obj/%.cxx.o: $(build_dir)/%.cxx
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(gen_cxx_objects:%.o=%.d)
@@ -314,7 +314,7 @@ endif
 
 # Generated cc files
 ifneq ("$(strip $(gen_cc_objects))","")
-$(gen_cc_objects): $(build_dir)/obj/%.o: $(build_dir)/%.cc
+$(gen_cc_objects): $(build_dir)/obj/%.cc.o: $(build_dir)/%.cc
 	$(transform-cpp-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(gen_cc_objects:%.o=%.d)
@@ -323,7 +323,7 @@ endif
 
 # Generated c files
 ifneq ("$(strip $(gen_c_objects))","")
-$(gen_c_objects): $(build_dir)/obj/%.o: $(build_dir)/%.c
+$(gen_c_objects): $(build_dir)/obj/%.c.o: $(build_dir)/%.c
 	$(transform-c-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(gen_c_objects:%.o=%.d)
@@ -333,14 +333,14 @@ endif
 # Generated s files
 # There is NO dependency files for raw asm code...
 ifneq ("$(strip $(gen_s_objects))","")
-$(gen_s_objects): $(build_dir)/obj/%.o: $(build_dir)/%.s
+$(gen_s_objects): $(build_dir)/obj/%.s.o: $(build_dir)/%.s
 	$(transform-s-to-o)
 endif
 
 # Generated S files
 # There is dependency files for asm code...
 ifneq ("$(strip $(gen_S_objects))","")
-$(gen_S_objects): $(build_dir)/obj/%.o: $(build_dir)/%.S
+$(gen_S_objects): $(build_dir)/obj/%.S.o: $(build_dir)/%.S
 	$(transform-s-to-o)
 ifneq ("$(skip_include_deps)","1")
 -include $(gen_S_objects:%.o=%.d)
