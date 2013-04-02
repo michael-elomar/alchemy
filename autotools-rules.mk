c35ebbc7cbecbd1864b7dd9e71db079f5c4d6464
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-04-02 13:22:54 +0200

    [DEV] Remove trace message.
    Change-Id: I71f878b0f577793e997c55498b04e93cf24e4cc7

--- autotools-rules.mk
+++ autotools-rules.mk
@@ -281,7 +281,6 @@ $(LOCAL_MODULE)-clean:
 # If the user makefile is changed, restart at the configure step
 # The touch of configure script ensures recompilation of parent dependencies
 ifneq ("$(configure_file)","")
-$(info $(LOCAL_MODULE):$(LOCAL_PATH)/$(USER_MAKEFILE_NAME))
 $(configure_file): $(LOCAL_PATH)/$(USER_MAKEFILE_NAME)
 	@touch $@
 endif
