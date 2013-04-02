44f6b6c666146e2bc4adf52bbb3f1ecd8379e949
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-04-02 10:18:43 +0200

    [DEV] Add LOCAL_REQUIRED_MODULES, alias for LOCAL_DEPENDS_MODULES.
    - This name ahas a better meaning that the old one.
    
    Change-Id: If1a0a95114684eaeb366486119a965129f0e756d

--- defs.mk
+++ defs.mk
@@ -469,7 +469,8 @@ __module-compute-depends-direct = \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.SHARED_LIBRARIES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.EXTERNAL_LIBRARIES)) \
 	$(eval __modules.$1.depends.headers := $(__modules.$1.DEPENDS_HEADERS)) \
-	$(eval __modules.$1.depends.other := $(__modules.$1.DEPENDS_MODULES))
+	$(eval __modules.$1.depends.other := $(__modules.$1.DEPENDS_MODULES)) \
+	$(eval __modules.$1.depends.other += $(__modules.$1.REQUIRED_MODULES))
 
 # Add direct dependencies to a module
 # $1 : module name.
