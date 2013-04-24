68b45c8886e3d5bffcdb4debce042580e5c7cec5
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-04-24 11:26:38 +0200

    [DEBUG] Do not use job control in alchemake when stdout is not a tty.
    - This fixes some strange behjaior when doing alchemake.py | less
    
    Change-Id: I8b2e8335622139e7a23674920f669e29eeb8bb24

--- scripts/alchemake.py
+++ scripts/alchemake.py
@@ -177,7 +177,7 @@ def main():
 	setupLog()
 
 	# If not on a terminal, do NOT use job control, simply execute make...
-	if not os.isatty(0):
+	if not os.isatty(0) or not os.isatty(1):
 		logging.warning("Not using job control")
 		process = subprocess.Popen(["make"] + sys.argv[1:], shell=False)
 		process.wait()
