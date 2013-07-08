2c43dd1fd39f77f97f5cad435d93b627e1be52fd
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-07-08 11:10:07 +0200

    Revert "[DEV] Sort modules in xml database."
    Variable __module is already sorted...
    
    This reverts commit e62025d279edb6e3d6887f50ee1774845e806b54.

--- dump-database.mk
+++ dump-database.mk
@@ -87,7 +87,7 @@ __dump-database-xml = \
 	) \
 	$(call __write-xml,</target>) \
 	$(call __write-xml,<modules>) \
-	$(foreach __mod,$(ALL_MODULES), \
+	$(foreach __mod,$(__modules), \
 		$(eval __build := $(if $(call is-module-in-build-config,$(__mod)),yes,no)) \
 		$(call __write-xml,$(space4)<module name='$(__mod)' build='$(__build)'>) \
 		$(foreach __field,$(modules-fields-depends) $(vars-LOCAL), \
