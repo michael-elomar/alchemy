cd71681576da54b9713392e3f41aa2efb098adce
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-07-08 11:14:47 +0200

    [DEV] Update scan of xml database.
    - Load TARGET_xxx variables.
    - Sort modules in iterator.
    
    Change-Id: Ie3978b638f94ef93fab5e7aa66608c8b9ed016c8

--- scripts/moduledb.py
+++ scripts/moduledb.py
@@ -6,6 +6,14 @@ import xml.dom.minidom
 
 #===============================================================================
 #===============================================================================
+def getNodeContent(node):
+	if node.childNodes is not None and len(node.childNodes) == 1:
+		return node.childNodes[0].nodeValue
+	else:
+		return ""
+
+#===============================================================================
+#===============================================================================
 class Module(object):
 	def __init__(self, moduleNode):
 		self.name = moduleNode.getAttribute("name")
@@ -16,9 +24,9 @@ class Module(object):
 			fieldName = fieldNode.getAttribute("name")
 			valueNodes = fieldNode.getElementsByTagName("value")
 			# Normally only one 'value' node
-			if valueNodes != None and len(valueNodes) == 1:
-				# get value in the first child of the 'value' node
-				fieldValue = valueNodes[0].childNodes[0].nodeValue
+			if valueNodes is not None and len(valueNodes) == 1:
+				# Get value in the first child of the 'value' node
+				fieldValue = getNodeContent(valueNodes[0])
 				self.fields[fieldName] = fieldValue
 
 	def __repr__(self):
@@ -29,6 +37,7 @@ class Module(object):
 class ModuleDb(object):
 	def __init__(self):
 		self._modules = {}
+		self.targetVars = {}
 
 	def append(self, module):
 		self._modules[module.name] = module
@@ -37,7 +46,7 @@ class ModuleDb(object):
 		return self._modules[key]
 
 	def __iter__(self):
-		return iter(self._modules.values())
+		return iter([self._modules[key] for key in sorted(self._modules.keys())])
 
 #===============================================================================
 #===============================================================================
@@ -45,8 +54,22 @@ def loadXml(xmlPath):
 	# Parse xml file
 	xmlDom = xml.dom.minidom.parse(xmlPath)
 
-	# Load modules
 	modules = ModuleDb()
+
+	# Load TARGET_xxx variables
+	targetNodes = xmlDom.documentElement.getElementsByTagName("target")
+	if targetNodes is not None and len(targetNodes) == 1:
+		varNodes = targetNodes[0].getElementsByTagName("var")
+		for varNode in varNodes:
+			varName = varNode.getAttribute("name")
+			valueNodes = varNode.getElementsByTagName("value")
+			# Normally only one 'value' node
+			if valueNodes is not None and len(valueNodes) == 1:
+				# Get value in the first child of the 'value' node
+				varValue = getNodeContent(valueNodes[0])
+				modules.targetVars[varName] = varValue
+
+	# Load modules
 	moduleNodes = xmlDom.documentElement.getElementsByTagName("module")
 	for moduleNode in moduleNodes:
 		modules.append(Module(moduleNode))
