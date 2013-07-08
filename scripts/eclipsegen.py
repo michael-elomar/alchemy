80cf0cb22ea090cbc18ae9ed448820adc648fe3d
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-07-08 11:26:59 +0200

    [DEV] Update eclipse generator.
    - Add global includes and flags.
    
    Change-Id: I2126e58b59fec85a7f8403278aeba6ae321833cf

--- scripts/eclipsegen.py
+++ scripts/eclipsegen.py
@@ -12,38 +12,55 @@ def genCdtProjectProperties(fd, modules, module):
 	# Get dependencies of given module
 	depends = [modules[dep] for dep in module.fields.get("depends", "").split()]
 
+	# Langauges to generate
 	languages = ["C++ Source File", "C Source File"]
+
+	# Helper for includes
+	def genIncludes(incPaths):
+		for incPath in incPaths.split():
+			fd.write("    <includepath>%s</includepath>\n" % incPath)
+
+	# Helper for macros
+	def genMacros(flags):
+		for flag in flags.split():
+			if not flag.startswith("-D"):
+				continue
+			if "=" in flag:
+				(name, value) = flag[2:].split("=", 1)
+			else:
+				(name, value) = (flag[2:], "")
+			fd.write("    <macro><name>%s</name><value>%s</value></macro>\n" %
+					(name, value))
+
+	# Header
 	fd.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 	fd.write("<cdtprojectproperties>\n")
 
+	# Includes
 	fd.write("<section name=\"org.eclipse.cdt.internal.ui.wizards.settingswizards.IncludePaths\">\n")
 	for language in languages:
-		fd.write("<language name=\"%s\">\n" % language)
+		fd.write("  <language name=\"%s\">\n" % language)
+		genIncludes(modules.targetVars.get("GLOBAL_C_INCLUDES", ""))
 		for dep in depends:
-			for incPath in dep.fields.get("EXPORT_C_INCLUDES", "").split():
-				fd.write("<includepath>%s</includepath>\n" % incPath)
-		fd.write("</language>\n")
+			genIncludes(dep.fields.get("EXPORT_C_INCLUDES", ""))
+		fd.write("  </language>\n")
 	fd.write("</section>\n")
 
+	# Macros
 	fd.write("<section name=\"org.eclipse.cdt.internal.ui.wizards.settingswizards.Macros\">\n")
 	for language in languages:
-		fd.write("<language name=\"%s\">\n" % language)
+		fd.write("  <language name=\"%s\">\n" % language)
+		genMacros(modules.targetVars.get("GLOBAL_CFLAGS", ""))
+		if language == "C++ Source File":
+			genMacros(modules.targetVars.get("GLOBAL_CXXFLAGS", ""))
 		for dep in depends:
-			cflags = dep.fields.get("EXPORT_CFLAGS", "").split()
-			cxxflags = dep.fields.get("EXPORT_CXXFLAGS", "").split()
-			flags = cflags + cxxflags if language == "C++ Source File" else cflags
-			for flag in flags:
-				if not flag.startswith("-D"):
-					continue
-				if "=" in flag:
-					(name, value) = flag[2:].split("=", 1)
-				else:
-					(name, value) = (flag[2:], "")
-				fd.write("<macro><name>%s</name><value>%s</value></macro>\n" %
-						(name, value))
-		fd.write("</language>\n")
+			genMacros(dep.fields.get("EXPORT_CFLAGS", ""))
+			if language == "C++ Source File":
+				genMacros(dep.fields.get("EXPORT_CXXFLAGS", ""))
+		fd.write("  </language>\n")
 	fd.write("</section>\n")
 
+	# Footer
 	fd.write("</cdtprojectproperties>\n")
 
 #===============================================================================
@@ -72,11 +89,11 @@ def parseArgs():
 	parser = optparse.OptionParser(usage=usage)
 
 	# Main options
-	parser.add_option("--force",
-		dest="force",
-		action="store_true",
-		default=False,
-		help="Overwrite existing files.")
+#	parser.add_option("--force",
+#		dest="force",
+#		action="store_true",
+#		default=False,
+#		help="Overwrite existing files.")
 
 	# Parse arguments and check validity
 	(options, args) = parser.parse_args()
