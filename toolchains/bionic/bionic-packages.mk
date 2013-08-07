21df2b71d504356bffdbde6d947a6f27136067db
Imen Toukabri <imen.toukabri@parrot.com>
2013-08-07 15:22:01 +0200

    [DEV]: Add busybox to prebuilt modules.

--- toolchains/bionic/bionic-packages.mk
+++ toolchains/bionic/bionic-packages.mk
@@ -90,3 +90,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libGLESv1_CM
 LOCAL_EXPORT_LDLIBS := -lGLESv1_CM
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := busybox
+include $(BUILD_PREBUILT)
+
