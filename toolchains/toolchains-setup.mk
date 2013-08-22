6b5bd7ba3e5487cd8a742c1561174c8536edea90
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-08-22 15:29:41 +0200

    [BUGFIX 119847] On android , the gcc version is not correctly parsed.
    - accept 'x' as 3rd digit of version.
    
    Change-Id: I4747528bb1c32b7a2f611ab17f51ef50daf143c4

--- toolchains/toolchains-setup.mk
+++ toolchains/toolchains-setup.mk
@@ -162,9 +162,9 @@ endif
 # Machine targetted by toolchain to be used by autotools
 TOOLCHAIN_TARGET_NAME ?= $(shell $(TARGET_CC) -dumpmachine)
 
-# Determine compiler version.
+# Determine compiler version (accept x for 3rd digit)
 ifneq ("$(USE_CLANG)","1")
-TARGET_CC_VERSION := $(shell $(TARGET_CC) --version | head -1 | sed "s/.*\([0-9]\.[0-9]\.[0-9]\).*/\1/")
+TARGET_CC_VERSION := $(shell $(TARGET_CC) --version | head -1 | sed "s/.*\([0-9]\.[0-9]\.[0-9x]\).*/\1/")
 else
 TARGET_CC_VERSION := $(shell $(TARGET_CC) --version | head -1 | sed "s/.*\([0-9]\.[0-9]\).*/\1/")
 endif
