325be0e3e36fcd136d071f9a3575ed5b1e7e5626
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2013-09-24 09:47:49 +0200

    [DEV] If a sdk has a setup.mk file, include it first.
    - This is needed for the sdk generated by the android build where a lot of
      TARGET_XXX variables are defined.

--- main.mk
+++ main.mk
@@ -163,6 +163,11 @@ $(foreach __var,$(vars-TARGET), \
 	) \
 )
 
+# If a sdk has a setup.mk file, include it
+$(foreach __dir,$(TARGET_SDK_DIRS), \
+	$(eval -include $(__dir)/setup.mk) \
+)
+
 # Setup configuration
 include $(BUILD_SYSTEM)/setup.mk
 
