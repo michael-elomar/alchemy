8d3734542e5c3ce3b6111bac1936ead9630f597b
Francois Ferrand <francois.ferrand@parrot.com>
2013-12-10 16:59:40 +0100

    [DEV] Import host libraries when making a native build.
    This does not apply to chroot'ed native build.
    
    Change-Id: I0c7ee9abcbc77b09e4770f70320da13622af7b30
    Reviewed-on: https://gerrit.parrot.biz/95
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- toolchains/native/native-packages.mk
+++ toolchains/native/native-packages.mk
@@ -6,3 +6,28 @@
 ## This file contains package definition specific to native linux.
 ###############################################################################
 
+ifeq ("$(TARGET_OS_FLAVOUR)","native")
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libusb
+LOCAL_EXPORT_LDLIBS := -lusb
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := alsa-lib
+LOCAL_EXPORT_LDLIBS := -lasound
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libudev
+LOCAL_EXPORT_LDLIBS := -ludev
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := zlib
+LOCAL_EXPORT_LDLIBS := -lz
+include $(BUILD_PREBUILT)
+
+endif
