aca4f0d482257f672c730cf3646b93e4a4ce7dc6
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2014-04-02 09:39:49 +0200

    [DEBUG] TARGET_INCLUDE_GDBSERVER test.
    Change-Id: Ifc11dd2d5aca1cbdb70dd05a8b7df136d42cfd33

--- final.mk
+++ final.mk
@@ -30,7 +30,7 @@ ifneq ("$(TOOLCHAIN_LIBC)","")
   MAKEFINAL_ARGS += --toolchain-libc="$(TOOLCHAIN_LIBC)"
 endif
 
-ifneq ("$(TARGET_INCLUDE_GDBSERVER)",0")
+ifneq ("$(TARGET_INCLUDE_GDBSERVER)","0")
 ifneq ("$(TOOLCHAIN_GDBSERVER)","")
   MAKEFINAL_ARGS += --toolchain-gdbserver="$(TOOLCHAIN_GDBSERVER)"
 endif
