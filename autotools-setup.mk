michael-elomar/alchemy

commit d9b2806e3134ef36fa3d4c3b466b9f4f585c2073

[DEBUG] Fix pkg-config and cmake path for native compilation
This patch allow pkg-config and cmake to search on host default system
folder for pkg-config lib and cmake module.

Change-Id: I8c38722cecf14c732feae248a0a49f5e85d0ac12
Reviewed-on: https://gerrit.parrot.biz/13835
Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

autotools-setup.mk
@@ -143,12 +143,12 @@ $(foreach __dir,$(TARGET_OUT_STAGING) $(TARGET_SDK_DIRS), \
 # Only use packages found in TARGET_OUT_STAGING by setting PKG_CONFIG_LIBDIR empty
 TARGET_PKG_CONFIG_ENV := \
 	PKG_CONFIG="$(__autotools-pkg-config-bin)" \
-	PKG_CONFIG_PATH="$(__target_pkg_config_path)" \
-	PKG_CONFIG_LIBDIR=""
+	PKG_CONFIG_PATH="$(__target_pkg_config_path)"
 ifeq ("$(TARGET_OS_FLAVOUR)","native")
   TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR=""
 else
   TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR="$(TARGET_OUT_STAGING)"
+  PKG_CONFIG_LIBDIR=""
 endif
 
 # Environment to use when executing configure script

cmake-setup.mk
@@ -43,6 +43,12 @@ endif
 ## Generation of toolchain file.
 ###############################################################################
 
+ifeq ("$(TARGET_OS_FLAVOUR)","native")
+  CMAKE_SEARCH_OPTION += BOTH
+else
+  CMAKE_SEARCH_OPTION += ONLY
+endif
+
 define cmake-gen-toolchain-file
 	echo "set(CMAKE_SYSTEM_NAME Linux)"; \
 	echo "set(CMAKE_SYSTEM_PROCESSOR \"$(TARGET_ARCH)\")"; \
@@ -65,8 +71,8 @@ define cmake-gen-toolchain-file
 	echo "set(CMAKE_INSTALL_SO_NO_EXE 0)"; \
 	echo "set(CMAKE_FIND_ROOT_PATH \"$(TARGET_OUT_STAGING)\")"; \
 	echo "set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)"; \
-	echo "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)"; \
-	echo "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)"; \
+	echo "set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY $(CMAKE_SEARCH_OPTION))"; \
+	echo "set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE $(CMAKE_SEARCH_OPTION))"; \
 	echo "set(CMAKE_COLOR_MAKEFILE OFF CACHE BOOL \"COLOR_MAKEFILE\" FORCE)"; \
 	echo "set(CMAKE_SKIP_INSTALL_RPATH ON CACHE BOOL \"SKIP_INSTALL_RPATH\" FORCE)";
 endef

commit 66372c50347822995d851bcc946fab9f06ab167d

[DEBUG] Fix autotools pkg-config libdir on non-native os flavour
Otherwise we will likely get some pkg-config from host.
This bug has been introduced by commit d9b2806e3134ef36fa3d4c3b466b9f4f585c2073.

Change-Id: I7db9a73bf6c61ae2f968e6a7a561c61b3dff7811
Reviewed-on: https://gerrit.parrot.biz/13863
Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

autotools-setup.mk
@@ -147,8 +147,8 @@ TARGET_PKG_CONFIG_ENV := \
 ifeq ("$(TARGET_OS_FLAVOUR)","native")
   TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR=""
 else
-  TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR="$(TARGET_OUT_STAGING)"
-  PKG_CONFIG_LIBDIR=""
+  TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR="$(TARGET_OUT_STAGING)" \
+			   PKG_CONFIG_LIBDIR=""
 endif
 
 # Environment to use when executing configure script