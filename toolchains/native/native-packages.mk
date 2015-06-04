0b4a59b8210e25f1bcdc8795d5eea1aa1b95c437
Frederic Chanal <frederic.chanal@parrot.com>
2015-06-04 11:59:17 +0200

    [DEV] Add more prebuilt lib for native build
    The added lib are all packages in parrot module, but the native version
    is best adapted when compiled on host.
    Generally this libs are not internally patched or modified so we shall
    expect same the behavior (but it is not guaranteed as the version
    changes)
    
    Change-Id: Iff499c4a7c549f831eb6153f50554be8ce22df01
    Reviewed-on: https://gerrit.parrot.biz/15164
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- toolchains/native/native-packages.mk
+++ toolchains/native/native-packages.mk
@@ -46,4 +46,42 @@ LOCAL_EXPORT_CFLAGS := $(shell pkg-config --cflags json)
 LOCAL_EXPORT_LDLIBS := $(shell pkg-config --libs json)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := glib
+LOCAL_EXPORT_CFLAGS := $(shell pkg-config --cflags glib-2.0 gobject-2.0 gio-2.0)
+LOCAL_EXPORT_LDLIBS := $(shell pkg-config --libs glib-2.0 gobject-2.0 gio-2.0)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := opengl
+LOCAL_EXPORT_CFLAGS := $(shell pkg-config --cflags glesv2)
+LOCAL_EXPORT_LDLIBS := $(shell pkg-config --libs glesv2)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libjpeg-turbo
+LOCAL_EXPORT_LDLIBS := -ljpeg
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtiff
+LOCAL_EXPORT_LDLIBS := -ltiff
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libpng
+LOCAL_EXPORT_CFLAGS := $(shell pkg-config --cflags libpng)
+LOCAL_EXPORT_LDLIBS := $(shell pkg-config --libs libpng)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := opencv
+LOCAL_EXPORT_LDLIBS := -lopencv_flann -lopencv_core -lopencv_imgproc    \
+	-lopencv_calib3d -lopencv_contrib -lopencv_features2d -lopencv_gpu  \
+	-lopencv_highgui -lopencv_legacy -lopencv_ml -lopencv_objdetect     \
+	-lopencv_ocl -lopencv_photo -lopencv_stitching -lopencv_superres 	\
+	-lopencv_ts -lopencv_video -lopencv_videostab
+
+include $(BUILD_PREBUILT)
+
 endif
