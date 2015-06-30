463ab8f42088f174adbeb8408e3cbbcdaf864e3b
Francois Ferrand <francois.ferrand@parrot.com>
2015-06-30 10:45:43 +0200

    [BUILD] Add missing boost-regex package.
    Change-Id: If67440a1f8642bb33a8b1988a1e1f6a37531db57
    Reviewed-on: https://gerrit.parrot.biz/16484
    Reviewed-by: Rémi Lefevre <remi.lefevre@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- toolchains/native/native-packages.mk
+++ toolchains/native/native-packages.mk
@@ -110,6 +110,11 @@ LOCAL_EXPORT_LDLIBS := -lboost_date_time
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libboost-regex
+LOCAL_EXPORT_LDLIBS := -lboost_regex
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libboost-thread
 LOCAL_LIBRARIES := libboost-system libboost-atomic libboost-chrono \
 	libboost-date-time
@@ -121,7 +126,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libboost
 LOCAL_LIBRARIES := libboost-system libboost-thread
 LOCAL_EXPORT_LDLIBS := -lboost_system -lboost_atomic -lboost_chrono \
-	-lboost_date_time -lboost_thread
+	-lboost_date_time -lboost_thread -lboost_regex
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
