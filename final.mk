080f8f04f5cd6168562698345dcab422b5623c72
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2015-07-26 20:42:40 +0200

    [DEBUG] Only put --strip-debug to kernel modules.
    - This will keep previous behaviour for user space strip.
    
    Change-Id: Ibff624f11aae40b6ba114e4ad6619e477303f11d

--- final.mk
+++ final.mk
@@ -18,11 +18,12 @@ endif
 
 MAKEFINAL_ARGS :=
 
-# Stripping kernel modules requires --strip-debug option
+# Stripping kernel modules requires --strip-debug option and its
+# specific strip program
 ifneq ("$(TARGET_STRIP)","")
 ifeq ("$(TARGET_NOSTRIP_FINAL)","0")
-  MAKEFINAL_ARGS += --strip="$(TARGET_STRIP) --strip-debug"
-  ifneq ("$(TARGET_LINUX_CROSS)","$(TARGET_CROSS)")
+  MAKEFINAL_ARGS += --strip="$(TARGET_STRIP)"
+  ifneq ("$(TARGET_LINUX_CROSS)","")
     MAKEFINAL_ARGS += --strip-kernel="$(TARGET_LINUX_CROSS)strip --strip-debug"
   endif
 endif
@@ -87,9 +88,11 @@ MAKEFINAL_ARGS += \
 	--build-id \
 	--build-id-objcopy="$(TARGET_CROSS)objcopy" \
 	--build-id-section-name="$(TARGET_BUILDID_SECTION_NAME)"
+ifneq ("$(TARGET_LINUX_CROSS)","")
 ifneq ("$(TARGET_LINUX_CROSS)","$(TARGET_CROSS)")
 MAKEFINAL_ARGS += \
 	--build-id-objcopy-kernel="$(TARGET_LINUX_CROSS)objcopy"
+endif
 endif
 endif
 
