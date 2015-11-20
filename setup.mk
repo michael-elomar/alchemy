be2386222759a3591ec7b63fbb553ce5e91dcab6
Francois Ferrand <francois.ferrand@parrot.com>
2015-11-20 15:44:23 +0100

    [DEV] Extract all target/path logic into a dedicated makefile.
    This allows this script to be reused from other (application's)
    makefile to properly integrate with Alchemy without re-doing what it
    already does: architecture selection, path, ...
    
    Change-Id: I14882cfa9ada1e1cb9c17d3175ab1feacddd3fcc
    Reviewed-on: https://gerrit.parrot.biz/25213
    Reviewed-by: Aymen Bouaziz <aymen.bouaziz@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- setup.mk
+++ setup.mk
@@ -15,90 +15,12 @@ $(error Top directory contains space characters)
 endif
 
 ###############################################################################
-## Host preliminary setup.
-###############################################################################
-HOST_OS := $(shell uname -s | awk '{print tolower($$0)}')
-HOST_CC ?= gcc
-
-# Architecture
-ifndef HOST_ARCH
-  ifneq ("$(shell $(HOST_CC) -dumpmachine | grep 64)","")
-    HOST_ARCH := x64
-  else
-    HOST_ARCH := x86
-  endif
-endif
-
-###############################################################################
-## Target OS aliases.
-###############################################################################
-
-# TARGET_OS aliases to simplify selection of android/iphone/iphonesimulator targets
-ifeq ("$(TARGET_OS)","android")
-  override TARGET_OS = linux
-  override TARGET_OS_FLAVOUR = android
-  TARGET_ARCH ?= arm
-else ifeq ("$(TARGET_OS)","parrot")
-  override TARGET_OS = linux
-  override TARGET_OS_FLAVOUR = parrot
-  TARGET_ARCH ?= arm
-else ifeq ("$(TARGET_OS)","iphone")
-  override TARGET_OS = darwin
-  override TARGET_OS_FLAVOUR = iphoneos
-  TARGET_ARCH ?= arm
-else ifeq ("$(TARGET_OS)","iphonesimulator")
-  override TARGET_OS = darwin
-  override TARGET_OS_FLAVOUR = iphonesimulator
-else ifeq ("$(TARGET_OS)","yocto")
-  override TARGET_OS = linux
-  override TARGET_OS_FLAVOUR = yocto
-  TARGET_ARCH ?= arm
-endif
-
-###############################################################################
 ## Target configuration.
-###############################################################################
 
-TARGET_ARCH ?= $(HOST_ARCH)
-TARGET_CPU ?=
-TARGET_OS ?= $(shell uname -s | awk '{print tolower($$0)}')
-TARGET_OS_FLAVOUR ?= native
-TARGET_LIBC ?=
-TARGET_PRODUCT ?= $(TARGET_OS)-$(TARGET_OS_FLAVOUR)
-TARGET_PRODUCT_VARIANT ?= $(TARGET_ARCH)
+include $(BUILD_SYSTEM)/envsetup.mk
 
-ifeq ("$(TARGET_PRODUCT_VARIANT)","")
-  TARGET_PRODUCT_FULL_NAME := $(TARGET_PRODUCT)
-else
-  TARGET_PRODUCT_FULL_NAME := $(TARGET_PRODUCT)-$(TARGET_PRODUCT_VARIANT)
-endif
-
-# Only TARGET_OUT should be specified, other will be impossible to override in
-# future versions
-ifneq ("$(TARGET_OUT_BUILD)","")
-$(warning TARGET_OUT_BUILD is set, only TARGET_OUT should be specified)
-endif
-ifneq ("$(TARGET_OUT_STAGING)","")
-$(warning TARGET_OUT_STAGING, only TARGET_OUT should be specified)
-endif
-ifneq ("$(TARGET_OUT_FINAL)","")
-$(warning TARGET_OUT_FINAL, only TARGET_OUT should be specified)
-endif
-ifneq ("$(TARGET_OUT_DOC)","")
-$(warning TARGET_OUT_DOC, only TARGET_OUT should be specified)
-endif
-
-TARGET_OUT_PREFIX ?= Alchemy-out/
-TARGET_OUT ?= $(TOP_DIR)/$(TARGET_OUT_PREFIX)$(TARGET_PRODUCT_FULL_NAME)
-TARGET_OUT_BUILD ?= $(TARGET_OUT)/build
-TARGET_OUT_DOC ?= $(TARGET_OUT)/doc
-TARGET_OUT_STAGING ?= $(TARGET_OUT)/staging
-TARGET_OUT_FINAL ?= $(TARGET_OUT)/final
 TARGET_SKEL_DIRS ?=
 TARGET_NOSTRIP_FINAL ?= 0
-
-TARGET_CONFIG_PREFIX ?= Alchemy-config/
-TARGET_CONFIG_DIR ?= $(TOP_DIR)/$(TARGET_CONFIG_PREFIX)$(TARGET_PRODUCT)-$(TARGET_PRODUCT_VARIANT)
 
 # Force compilation of all modules as static (disable shared libraries)
 TARGET_FORCE_STATIC ?= 0
@@ -113,18 +35,6 @@ endif
 # Register list of tags used by a module. It can be retrieved at run time with
 # pal function 'pal_lib_desc_get_table_entry'
 TARGET_PBUILD_HOOK_USE_DESCRIBE ?= 0
-
-# Extra directories to skip/add during makefile scan
-TARGET_SCAN_PRUNE_DIRS ?=
-TARGET_SCAN_ADD_DIRS ?=
-
-# Ignore config and out directorie(s)
-ifneq ("$(dir $(TOP_DIR)/$(TARGET_CONFIG_PREFIX))","$(TOP_DIR)")
-TARGET_SCAN_PRUNE_DIRS += $(dir $(TOP_DIR)/$(TARGET_CONFIG_PREFIX))
-endif
-ifneq ("$(dir $(TOP_DIR)/$(TARGET_OUT_PREFIX))","$(TOP_DIR)")
-TARGET_SCAN_PRUNE_DIRS += $(dir $(TOP_DIR)/$(TARGET_OUT_PREFIX))
-endif
 
 # Set to 1 to follow symbolic links during scan
 TARGET_SCAN_FOLLOW_LINKS ?= 0
