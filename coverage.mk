f1e6d13b4a25b916cdb648ef63a2d9f90a899db2
Pierre Zurek <pierre.zurek@parrot.com>
2015-11-24 17:52:50 +0100

    [DEV] Allow customizing the gcov folder
    This commit adds the TARGET_OUT_GCOV variable to customize the gcov
    folder.
    For example, to put the .gcno files inside final/data/lib/gcov, one can
    set TARGET_OUT_GCOV to $(TARGET_OUT)/staging/data/lib/gcov in the
    product.mk file.
    
    Change-Id: I38f071189d49a8faf11b4af2ce6ebc7ee1d9e1bd
    Reviewed-on: https://gerrit.parrot.biz/25386
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- coverage.mk
+++ coverage.mk
@@ -39,8 +39,8 @@ coverage-copy-gcno:
 	@echo "Copying gcno files..."
 	@( \
 		for f in $$(cd $(TARGET_OUT_BUILD) && find -name '*.gcno'); do \
-			mkdir -p $$(dirname $(TARGET_OUT)/gcov/$$f); \
-			cp -af $(TARGET_OUT_BUILD)/$$f $(TARGET_OUT)/gcov/$$f; \
+			mkdir -p $$(dirname $(TARGET_OUT_GCOV)/$$f); \
+			cp -af $(TARGET_OUT_BUILD)/$$f $(TARGET_OUT_GCOV)/$$f; \
 		done \
 	)
 	@echo "Done copying gcno files"
@@ -48,7 +48,7 @@ coverage-copy-gcno:
 # Clean gcov directory
 .PHONY: coverage-copy-gcno-clean
 coverage-copy-gcno-clean:
-	$(Q) rm -rf $(TARGET_OUT)/gcov
+	$(Q) rm -rf $(TARGET_OUT_GCOV)
 
 # Setup dependencies
 coverage-copy-gcno: post-build
