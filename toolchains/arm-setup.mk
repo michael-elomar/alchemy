86caeb5ccfac2d5701f03e09a5f37442ebfea6b5
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-02-29 17:47:10 +0100

    [DEV] Add armv7a cpu (without neon)
    Change-Id: If36fcd4a4c6a744c8d3dfa3615e0c395436c9961

--- toolchains/arm-setup.mk
+++ toolchains/arm-setup.mk
@@ -100,6 +100,13 @@ ifeq ("$(TARGET_CPU)","omap4")
   TARGET_FLOAT_ABI ?= softfp
 endif
 
+# generic armv7a (without neon)
+ifeq ("$(TARGET_CPU)","armv7a")
+  TARGET_GLOBAL_CFLAGS += -march=armv7-a
+  TARGET_GLOBAL_LDFLAGS += -Wl,--fix-cortex-a8
+  TARGET_FLOAT_ABI ?= softfp
+endif
+
 # generic armv7a-neon
 ifeq ("$(TARGET_CPU)","armv7a-neon")
   TARGET_GLOBAL_CFLAGS += $(cflags_armv7a_neon)
