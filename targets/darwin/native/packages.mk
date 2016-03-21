5952e9427b5aec03c37ea8c1f74044272f861f0a
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-03-21 17:12:18 +0100

    [DEV] Add missing LOCAL_PATH in darwin/native packages
    Change-Id: I1fc547250938e8c275ed4048846bb593716df82f

--- targets/darwin/native/packages.mk
+++ targets/darwin/native/packages.mk
@@ -6,6 +6,8 @@
 ## Additional packages for darwin/native target.
 ###############################################################################
 
+LOCAL_PATH := $(call my-dir)
+
 $(call register-prebuilt-pkg-config-module,libusb,libusb)
 $(call register-prebuilt-pkg-config-module,libusb_1_0,libusb-1.0)
 $(call register-prebuilt-pkg-config-module,json,json-c)
