75055b1ab855f3faa78af8aa10b1f08f32cb10be
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-03-22 09:54:21 +0100

    [DEV] Allow override of darwin toolchain
    Change-Id: I7a763818705bbda349dbb5a4ccfb178f3fcc8ee9

--- toolchains/darwin/selection.mk
+++ toolchains/darwin/selection.mk
@@ -18,14 +18,36 @@ else ifeq ("$(TARGET_ARCH)","arm")
 endif
 
 TARGET_CROSS ?=
-TARGET_CC := $(shell xcrun --find --sdk $(APPLE_SDK) clang)
-TARGET_CXX := $(shell xcrun --find --sdk $(APPLE_SDK) clang++)
-TARGET_AS := $(shell xcrun --find --sdk $(APPLE_SDK) as)
-TARGET_AR := $(BUILD_SYSTEM)/scripts/darwin-ar $(shell xcrun --find --sdk $(APPLE_SDK) ar)
-TARGET_LD := $(shell xcrun --find --sdk $(APPLE_SDK) ld)
-TARGET_NM := $(shell xcrun --find --sdk $(APPLE_SDK) nm)
-TARGET_STRIP := $(shell xcrun --find --sdk $(APPLE_SDK) strip)
-TARGET_CPP := $(shell xcrun --find --sdk $(APPLE_SDK) cpp)
-TARGET_RANLIB := $(shell xcrun --find --sdk $(APPLE_SDK) ranlib)
-TARGET_OBJCOPY := $(TARGET_CROSS)objcopy	#TODO: use lipo wrapper....
-TARGET_OBJDUMP := $(TARGET_CROSS)objdump	#TODO: use otool wrapper....
+
+# Avoid using ?= when invoking shell to make sure we have an immediate expansion
+ifndef TARGET_CC
+  TARGET_CC := $(shell xcrun --find --sdk $(APPLE_SDK) clang)
+endif
+ifndef TARGET_CXX
+  TARGET_CXX := $(shell xcrun --find --sdk $(APPLE_SDK) clang++)
+endif
+ifndef TARGET_AS
+  TARGET_AS := $(shell xcrun --find --sdk $(APPLE_SDK) as)
+endif
+ifndef TARGET_AR
+  TARGET_AR := $(BUILD_SYSTEM)/scripts/darwin-ar $(shell xcrun --find --sdk $(APPLE_SDK) ar)
+endif
+ifndef TARGET_LD
+  TARGET_LD := $(shell xcrun --find --sdk $(APPLE_SDK) ld)
+endif
+ifndef TARGET_NM
+  TARGET_NM := $(shell xcrun --find --sdk $(APPLE_SDK) nm)
+endif
+ifndef TARGET_STRIP
+  TARGET_STRIP := $(shell xcrun --find --sdk $(APPLE_SDK) strip)
+endif
+ifndef TARGET_CPP
+  TARGET_CPP := $(shell xcrun --find --sdk $(APPLE_SDK) cpp)
+endif
+ifndef TARGET_RANLIB
+  TARGET_RANLIB := $(shell xcrun --find --sdk $(APPLE_SDK) ranlib)
+endif
+
+#TODO: use lipo wrapper....
+TARGET_OBJCOPY ?= $(TARGET_CROSS)objcopy
+TARGET_OBJDUMP ?= $(TARGET_CROSS)objdump
