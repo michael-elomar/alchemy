96386364b03306dc695f37c9ca11cff23974c74e
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-03-27 20:05:12 +0200

    [DEV] Cleanup.
    - Remove old AUTOTOOLS_CMD and CMAKE_CMD variables from macros-LOCAL
      They are still accepted in atom.mk for compatibility.
    - Remove add-buildid-section, not used anymore, section is added during
      make final step.
    
    Change-Id: I375c89a2912c8d2b2c9cd949a3624a0c561448ce

--- defs.mk
+++ defs.mk
@@ -1399,21 +1399,11 @@ check-custom-macro = \
 ## message displayed when used while TARGET_DEFAULT_ARM_MODE is 'arm'.
 ###############################################################################
 
-# Old variables still suported but no more in vars-LOCAL or macros-LOCAL
-__compat-vars-LOCAL := \
-	AUTOTOOLS_ARCHIVE \
-	AUTOTOOLS_VERSION \
-	AUTOTOOLS_SUBDIR \
-	AUTOTOOLS_PATCHES \
-	AUTOTOOLS_CMD_UNPACK \
-	AUTOTOOLS_CMD_POST_UNPACK \
-	AUTOTOOLS_COPY_TO_BUILD_DIR \
-
 # All allowed variables
 __all-vars-LOCAL := \
 	$(vars-LOCAL) \
 	$(macros-LOCAL) \
-	$(__compat-vars-LOCAL)
+	$(compat-vars-LOCAL)
 
 # Get all defined LOCAL_XXX variables
 __get-defined-local-vars = $(filter LOCAL_%,$(.VARIABLES))
@@ -1518,24 +1508,6 @@ $(eval __depsdata := $(subst $(space),\n,$(strip $(__depsdata))))
 	$(PRIVATE_OBJCOPY) --add-section \
 		$(TARGET_DEPENDS_SECTION_NAME)=$${__tmpfile} $@; \
 	rm -f $${__tmpfile}; \
-)
-endef
-
-###############################################################################
-## Add a section in a binary with a build id.
-## This is a `sha1` of all sections that are loadable at runtime and with data
-## in the binary. This section is kept after stripping and the `sha1` can be
-## recomputed on the original and stripped binary and still produce the same
-## `sha1`. This can be used to identify the non-stripped binary based on the
-## stripped one.
-## Note : shall only be used in a rule because it uses $@
-###############################################################################
-
-define add-buildid-section
-@( \
-	$(BUILD_SYSTEM)/scripts/addbuildid.py \
-	--objcopy=$(PRIVATE_OBJCOPY) \
-	--section-name=$(TARGET_BUILDID_SECTION_NAME) $@ \
 )
 endef
 
