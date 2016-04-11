d5637512eed410952ba270a12d5351725ab7b2f0
Frederic Chanal <frederic.chanal@parrot.com>
2016-04-11 15:59:21 +0200

    [DEBUG] Add some filtered CXXFLAGS to NVCC
    Change-Id: If6a99880e6969b28fe4076fbd6ed8e923b70f069
    Reviewed-on: https://gerrit.parrot.biz/35179
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- binary-rules.mk
+++ binary-rules.mk
@@ -405,9 +405,12 @@ endif
 
 # Nvcc flags
 # Remove from standard CFLAGS unsuported flags
+# Fix some syntax difference (like for "-std" flag)
 # Give filtered flags directly to compiler (with -Xcompiler prefix)
-__nvcflags-all := $(TARGET_GLOBAL_CFLAGS) $(TARGET_GLOBAL_CFLAGS_$(TARGET_ARCH)) $(LOCAL_CFLAGS)
-__nvcflags-1 := $(filter-out -pthread -pipe -f% -m% -O%, $(__nvcflags-all))
+__nvcflags-all := $(TARGET_GLOBAL_CFLAGS) $(TARGET_GLOBAL_CFLAGS_$(TARGET_ARCH)) $(LOCAL_CFLAGS) \
+	$(TARGET_GLOBAL_CXXFLAGS) $(TARGET_GLOBAL_CXXFLAGS_$(TARGET_ARCH)) $(LOCAL_CXXFLAGS)
+__nvcflags-1 := $(subst -std,--std,$(__nvcflags-all))
+__nvcflags-1 := $(filter-out -pthread -pipe -f% -m% -O% -W%, $(__nvcflags-1))
 __nvcflags-2 := $(addprefix -Xcompiler ,$(filter -pthread -pipe -f% -m% -O%, $(__nvcflags-all)))
 $(LOCAL_TARGETS): PRIVATE_NVCFLAGS := $(__nvcflags-1) $(__nvcflags-2)
 
