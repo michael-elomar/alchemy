74562e634f0f42c8cc4cdeee6b1620d5b6c5d07e
Rémi Lefèvre <remi.lefevre@parrot.com>
2016-06-08 18:40:06 +0200

    [DEV] Update default toolchain for aarch64 to linaro 2016.02-aarch64
    Change-Id: I96f856b5d8909aac226cf074948b789bcc04891f
    Reviewed-on: https://gerrit.parrot.biz/40726
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>
    Reviewed-by: Rémi Lefevre <remi.lefevre@parrot.com>

--- toolchains/linux/eglibc/selection.mk
+++ toolchains/linux/eglibc/selection.mk
@@ -18,7 +18,7 @@ ifndef TARGET_CROSS
       TARGET_CROSS := /opt/arm-2012.03/bin/arm-none-linux-gnueabi-
     endif
   else ifeq ("$(TARGET_ARCH)","aarch64")
-      TARGET_CROSS := /opt/arm-2014.11-aarch64-linaro/bin/aarch64-linux-gnu-
+      TARGET_CROSS := /opt/arm-2016.02-aarch64-linaro/bin/aarch64-linux-gnu-
   endif
 endif
 
