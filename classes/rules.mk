03a49c73784f4959747c6fba487de338857a5784
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2017-01-05 11:44:29 +0100

    [DEV] Allow android amv7a to override mfpu to neon.
    - To be compatible with some devices, some products are compiled with cpu
      as armv7a (without neon) but with some module compiled with neon
      support detected at runtime.
    
    Change-Id: Ia24daec8173efda69f0746a424847ccd9669f379
    Reviewed-on: https://gerrit.parrot.biz/59914
    Reviewed-by: David Flattin <david.flattin@parrot.com>
    Reviewed-by: Julien Thomas <julien.thomas@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- classes/rules.mk
+++ classes/rules.mk
@@ -168,13 +168,16 @@ $(call check-flags,LOCAL_EXPORT_CXXFLAGS,$(check-flags-debug),$(check-flags-debu
 
 # Forbid module to tweak architecture/cpu flags
 # They shall come from alchemy or product in TARGET_XXX variables
-check-flags-arch-cpu := -march=% -mcpu=% -mtune=% -mfloat-abi=%
+check-flags-arch-cpu := -march=% -mcpu=% -mtune=%
 check-flags-arch-cpu-message := please let alchemy or product determine arch/cpu flags
 
 # Unfortunately, there is one use case where a module overwrites the -mfpu=
 # due to a bug in 2012 toolchain
+# Also allow android armv7a to specify neon (can break at runtime if wrongly used)
 ifeq ("$(call str-starts-with,$(TARGET_CC_PATH),/opt/arm-2012.03)","")
-  check-flags-arch-cpu += -mfpu=%
+ifneq ("$(TARGET_OS)-$(TARGET_OS_FLAVOUR)-$(TARGET_CPU)","linux-android-armv7a")
+  check-flags-arch-cpu += -mfloat-abi=% -mfpu=%
+endif
 endif
 
 $(call check-flags,LOCAL_CFLAGS,$(check-flags-arch-cpu),$(check-flags-arch-cpu-message))
