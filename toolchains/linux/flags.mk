5fb32ee09a47fa693266f558dca80eaf520f7795
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2017-03-22 11:03:03 +0100

    [DEBUG] gnu hash not supported by mips ABI
    Change-Id: I06616014b2c8752bc2354db0c636d3e6e23de817
    Reviewed-on: https://gerrit.parrot.biz/67077
    Reviewed-by: Mathieu Vinel <mathieu.vinel@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- toolchains/linux/flags.mk
+++ toolchains/linux/flags.mk
@@ -11,4 +11,11 @@ ifneq ("$(TARGET_LIBC)","")
 endif
 
 # Enable link optimization for binutils's ld.
-TARGET_GLOBAL_LDFLAGS += -Wl,-O1,--hash-style=both
+# gnu hash not supported by mips ABI
+ifeq ("$(TARGET_ARCH)","mips")
+  TARGET_GLOBAL_LDFLAGS += -Wl,-O1
+else ifeq ("$(TARGET_ARCH)","mips64")
+  TARGET_GLOBAL_LDFLAGS += -Wl,-O1
+else
+  TARGET_GLOBAL_LDFLAGS += -Wl,-O1,--hash-style=both
+endif
