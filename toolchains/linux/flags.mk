a9bee09c4abd2fa9a9ba6d4a7fda92d4fe5ede25
Jimmy Perchet <jimmy.perchet@parrot.com>
2017-04-04 15:12:00 +0200

    [DEBUG] Keep linker full optimization to eglibc
    Change-Id: Ibb23b5283daefec0860ca96213a3f662dc7a76cb
    Reviewed-on: https://gerrit.parrot.biz/68288
    Reviewed-by: Aymen Bouaziz <aymen.bouaziz@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- toolchains/linux/flags.mk
+++ toolchains/linux/flags.mk
@@ -9,13 +9,3 @@
 ifneq ("$(TARGET_LIBC)","")
   -include $(BUILD_SYSTEM)/toolchains/$(TARGET_OS)/$(TARGET_LIBC)/flags.mk
 endif
-
-# Enable link optimization for binutils's ld.
-# gnu hash not supported by mips ABI
-ifeq ("$(TARGET_ARCH)","mips")
-  TARGET_GLOBAL_LDFLAGS += -Wl,-O1
-else ifeq ("$(TARGET_ARCH)","mips64")
-  TARGET_GLOBAL_LDFLAGS += -Wl,-O1
-else
-  TARGET_GLOBAL_LDFLAGS += -Wl,-O1,--hash-style=both
-endif
