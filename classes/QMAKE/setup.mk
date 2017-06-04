8b634fe6f99e1a8078d2e1d50b9edc207a14a8cf
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2017-06-04 22:38:01 +0200

    [DEV] Override installation commands used by qmake under windows
    - Preserve timestamps during copy to avoid unnecessary rebuilds
    
    Change-Id: I1a2f182777d8976a8ac706332453895ad1f524a3
    Reviewed-on: https://gerrit-doc.parrot.biz/72879
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- classes/QMAKE/setup.mk
+++ classes/QMAKE/setup.mk
@@ -184,6 +184,11 @@ define _internal-qmake-gen-deps
 		$(if $(call streq,$(TARGET_OS_FLAVOUR),android), \
 			echo "ANDROID_EXTRA_LIBS = $(shell find $(TARGET_OUT_STAGING)/$(TARGET_DEFAULT_LIB_DESTDIR) -maxdepth 1 -name 'lib*.so' -type f)"; \
 		) \
+		$(if $(call streq,$(HOST_OS),windows), \
+			echo "QMAKE_INSTALL_FILE = cp -dpf"; \
+			echo "QMAKE_INSTALL_PROGRAM = cp -dpf"; \
+			echo "QMAKE_INSTALL_DIR = cp -Rdpf"; \
+		) \
 	) >> $(PRIVATE_ALCHEMY_PRI_FILE).tmp
 	$(call update-file-if-needed,$(PRIVATE_ALCHEMY_PRI_FILE),$(PRIVATE_ALCHEMY_PRI_FILE).tmp)
 endef
