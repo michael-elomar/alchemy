michael-elomar/alchemy

commit 03b05c672e13443ae11f3fc7c5c0e7c1ba11474b

[DEV] Recurse inside META_PACKAGE and PREBUILT to find dependencies.
- If A depends on B which is either META_PACKAGE or PREBUILT, it will
  give to A all exported flags and libs found in dependencies of B as
  well.

Change-Id: I495c74bab55b989ed73d2f80b1f2c786e729c16f
Reviewed-on: https://gerrit.parrot.biz/72876
Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

classes/rules.mk
@@ -217,6 +217,8 @@ $(call check-flags,LOCAL_EXPORT_CXXFLAGS,$(check-flags-arch-cpu),$(check-flags-a
 ###############################################################################
 
 # Get libraries used by us and static libraries
+all_prebuilt_libs := \
+	$(call module-get-static-depends,$(LOCAL_MODULE),PREBUILT_LIBRARIES)
 all_external_libs := \
 	$(call module-get-static-depends,$(LOCAL_MODULE),EXTERNAL_LIBRARIES)
 all_static_libs := \
@@ -228,6 +230,7 @@ all_shared_libs := \
 
 # List of our dependencies and from static (recursive on static libs)
 all_libs := \
+	$(all_prebuilt_libs) \
 	$(all_external_libs) \
 	$(all_static_libs) \
 	$(all_whole_static_libs) \

defs.mk
@@ -224,6 +224,8 @@ clear-vars = $(foreach __varname,$1,$(eval $(__varname) := $(empty)))
 ###############################################################################
 modules-fields-depends := \
 	depends \
+	depends.META_PACKAGES \
+	depends.PREBUILT_LIBRARIES \
 	depends.EXTERNAL_LIBRARIES \
 	depends.STATIC_LIBRARIES \
 	depends.WHOLE_STATIC_LIBRARIES \
@@ -681,7 +683,7 @@ modules-compute-depends = \
 		$(call __module-compute-depends-direct,$(__mod)) \
 	) \
 	$(foreach __mod,$(__modules), \
-		$(foreach __field,EXTERNAL_LIBRARIES STATIC_LIBRARIES WHOLE_STATIC_LIBRARIES SHARED_LIBRARIES, \
+		$(foreach __field,META_PACKAGES PREBUILT_LIBRARIES EXTERNAL_LIBRARIES STATIC_LIBRARIES WHOLE_STATIC_LIBRARIES SHARED_LIBRARIES, \
 			$(eval __depends-loop := $(empty)) \
 			$(eval __dummy := $(call __module-compute-depends-static,$(__mod),$(__field))) \
 		) \
@@ -713,14 +715,22 @@ __module-update-depends-direct = \
 				$(eval __modules.$1.WHOLE_STATIC_LIBRARIES += $(__lib)), \
 				$(eval __modules.$1.STATIC_LIBRARIES += $(__lib)) \
 			) \
-		, \
+			, \
 			$(if $(call streq,$(__class),SHARED_LIBRARY), \
 				$(eval __modules.$1.SHARED_LIBRARIES += $(__lib)) \
-			, \
+				, \
 				$(if $(call streq,$(__class),LIBRARY), \
 					$(eval __modules.$1.SHARED_LIBRARIES += $(__lib)) \
 					, \
-					$(eval __modules.$1.EXTERNAL_LIBRARIES += $(__lib)) \
+					$(if $(call streq,$(__class),META_PACKAGE), \
+						$(eval __modules.$1.META_PACKAGES += $(__lib)) \
+						, \
+						$(if $(call streq,$(__class),PREBUILT), \
+							$(eval __modules.$1.PREBUILT_LIBRARIES += $(__lib)) \
+							, \
+							$(eval __modules.$1.EXTERNAL_LIBRARIES += $(__lib)) \
+						) \
+					) \
 				) \
 			) \
 		) \
@@ -732,6 +742,8 @@ __module-compute-depends-direct = \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.STATIC_LIBRARIES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.WHOLE_STATIC_LIBRARIES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.SHARED_LIBRARIES)) \
+	$(call __module-add-depends-direct,$1,$(__modules.$1.META_PACKAGES)) \
+	$(call __module-add-depends-direct,$1,$(__modules.$1.PREBUILT_LIBRARIES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.EXTERNAL_LIBRARIES)) \
 	$(eval __modules.$1.depends.headers := $(__modules.$1.DEPENDS_HEADERS)) \
 	$(eval __modules.$1.depends.build += $(__modules.$1.DEPENDS_MODULES)) \
@@ -784,6 +796,14 @@ __module-compute-depends-static-internal = \
 		) \
 	)
 
+# Recurse prebuilt and meta packages as well
+__module-compute-depends-static-internal += \
+	$(foreach __mod,$(__modules.$1.META_PACKAGES) $(__modules.$1.PREBUILT_LIBRARIES), \
+		$(if $(call is-module-registered,$(__mod)), \
+			$(call __module-compute-depends-static,$(__mod),$2) \
+		) \
+	)
+
 # When forcing static libraries, take into account external libraries as well
 # Otherwise assume they are mostly shared libraries
 __module-compute-depends-static-internal += \
@@ -799,6 +819,8 @@ __module-compute-depends-static-internal += \
 # $1 : module name.
 __module-compute-depends-link = \
 	$(eval __modules.$1.depends.link := $(strip $(sort \
+		$(__modules.$1.depends.META_PACKAGES) \
+		$(__modules.$1.depends.PREBUILT_LIBRARIES) \
 		$(__modules.$1.depends.EXTERNAL_LIBRARIES) \
 		$(__modules.$1.depends.STATIC_LIBRARIES) \
 		$(__modules.$1.depends.WHOLE_STATIC_LIBRARIES) \

variables.mk
@@ -68,6 +68,12 @@ vars-LOCAL += SHARED_LIBRARIES
 # Used as dependencies to trigger indirect build.
 vars-LOCAL += EXTERNAL_LIBRARIES
 
+# Prebuilt libraries
+vars-LOCAL += PREBUILT_LIBRARIES
+
+# Meta packages
+vars-LOCAL += META_PACKAGES
+
 # General libraries to add in dependency based on their actual class (STATIC/SHARED/EXTERNAL).
 vars-LOCAL += LIBRARIES
 

commit ded847830dbf42072c99f5ee3cfc9b473fd441a4

[DEBUG] Add missing export from META_PACKAGES
- Regression from 03b05c672e13443ae11f3fc7c5c0e7c1ba11474b
  Recurse inside META_PACKAGE and PREBUILT to find dependencies.

Change-Id: I9506e1de32e063f35556023d0e05b8e420ecd6c2
Reviewed-on: https://gerrit.parrot.biz/73027
Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

classes/rules.mk
@@ -217,6 +217,8 @@ $(call check-flags,LOCAL_EXPORT_CXXFLAGS,$(check-flags-arch-cpu),$(check-flags-a
 ###############################################################################
 
 # Get libraries used by us and static libraries
+all_meta_packages := \
+	$(call module-get-static-depends,$(LOCAL_MODULE),META_PACKAGES)
 all_prebuilt_libs := \
 	$(call module-get-static-depends,$(LOCAL_MODULE),PREBUILT_LIBRARIES)
 all_external_libs := \
@@ -230,6 +232,7 @@ all_shared_libs := \
 
 # List of our dependencies and from static (recursive on static libs)
 all_libs := \
+	$(all_meta_packages) \
 	$(all_prebuilt_libs) \
 	$(all_external_libs) \
 	$(all_static_libs) \