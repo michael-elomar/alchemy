8dfefe765982f81046ad40c5110bf57c5d896ca0
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2017-09-25 16:57:16 +0200

    [DEBUG] Always apply fixstat if permission files are given
    Change-Id: Ie03bbb54b9d7860c38001fc813b5b246f0391c7f
    Reviewed-on: https://gerrit-doc.parrot.biz/79024
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- image.mk
+++ image.mk
@@ -12,18 +12,13 @@ SPARSE_SCRIPT := $(BUILD_SYSTEM)/scripts/sparse.py
 # Script that will modify mode/uid/gid of files while generating the image
 FIXSTAT := $(BUILD_SYSTEM)/scripts/fixstat.py
 
-ifeq ("$(TARGET_OS)","linux")
-ifeq ("$(is-full-system)","1")
+# Apply default permissions (quite restrictives) only if other rules are present
+ifneq ("$(TARGET_PERMISSIONS_FILES)","")
 FIXSTAT += \
+	--use-default \
 	--user-file=$(TARGET_OUT_FINAL)/$(TARGET_DEFAULT_ETC_DESTDIR)/passwd \
 	--group-file=$(TARGET_OUT_FINAL)/$(TARGET_DEFAULT_ETC_DESTDIR)/group \
 	$(foreach __f,$(TARGET_PERMISSIONS_FILES), --permissions-file=$(__f))
-endif
-endif
-
-# Apply default permissions (quite restrictives) only if other rules are present
-ifneq ("$(TARGET_PERMISSIONS_FILES)","")
-  FIXSTAT += --use-default
 endif
 
 ifneq ("$(V)","0")
