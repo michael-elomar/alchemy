7c17ee19c0e51ead6e1404538f3825fe1fcbfa03
Matthieu CASTET <matthieu.castet@parrot.com>
2017-10-19 18:04:24 +0200

    [DEV] make TARGET_NOSTRIP_FINAL=2 when building single target
    Without this commit "./build.sh xxxx -A target" produce a stripped
    binary.
    
    Change-Id: I3fa7b1ae4533d9ec3b43ed7d5eec536e92f27d14
    Reviewed-on: https://gerrit-doc.parrot.biz/80551
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- final.mk
+++ final.mk
@@ -23,13 +23,7 @@ MAKEFINAL_ARGS :=
 # specific strip program
 ifneq ("$(TARGET_NOSTRIP_FINAL)","1")
   ifneq ("$(TARGET_STRIP)","")
-    #strip only debug info but keep symbol table for symbol resolving on target
-    #this usefull for tools like perf
-    ifeq ("$(TARGET_NOSTRIP_FINAL)","2")
-      MAKEFINAL_ARGS += --strip="$(TARGET_STRIP) --strip-debug"
-    else
       MAKEFINAL_ARGS += --strip="$(TARGET_STRIP)"
-    endif
   endif
   ifeq ("$(TARGET_OS)","linux")
     ifneq ("$(TARGET_LINUX_CROSS)","")
