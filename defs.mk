ce19372b0f483473066f69afb97a13d34c7d7eda
Nicolas BRULEZ <nicolas.brulez@parrot.com>
2018-06-05 15:39:07 +0200

    [DEBUG] Remove extra parenthesis in 'is-module-in-make-goals' macro
    Change-Id: I0dd7f0dc72c4eecf9ee5addaab43c2286e5872a6
    Reviewed-on: https://gerrit-doc.parrot.biz/93231
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- defs.mk
+++ defs.mk
@@ -352,7 +352,7 @@ is-targets-in-make-goals = $(strip \
 ###############################################################################
 is-module-in-make-goals = $(strip \
 	$(call is-targets-in-make-goals,$1 $1-clean $1-dirclean $1-path $1-cloc $1-doc \
-		$(call codecheck-get-targets,$1)) \
+		$(call codecheck-get-targets,$1) \
 		$(call genproject-get-targets,$1)))
 
 ###############################################################################
