f68be8fb301c6690d7ceb3e24f1f131a447d1d4a
Hugo Grostabussiat <hugo.grostabussiat@parrot.com>
2018-11-19 09:50:07 +0100

    [DEBUG] Fix compatibility with pkgconf
    By default, pkg-config looks for .pc files in its primary directory on
    the host. This directory can be overridden with the environment variable
    PKG_CONFIG_LIBDIR.
    
    With pkg-config, setting this variable to the empty string is enough to
    remove the default libdir from the list of primary directories.
    However, with pkgconf, another implementation of pkg-config, doing so
    does not have any effect (tested with pkgconf 1.5.4).
    
    This patch sets the PKG_CONFIG_LIBDIR environment variable to the
    staging dirextory, where we can reasonably expect that no .pc files from
    the host will be present.
    
    Change-Id: I8d20c5a6d2d74d769494f7cd5df557d4b2b37d39
    Reviewed-on: https://gerrit-doc.parrot.biz/101264
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- classes/AUTOTOOLS/setup.mk
+++ classes/AUTOTOOLS/setup.mk
@@ -273,7 +273,9 @@ $(foreach __dir,$(TARGET_OUT_STAGING) $(TARGET_SDK_DIRS), \
 )
 
 # Setup pkg-config
-# Only use packages found in TARGET_OUT_STAGING by setting PKG_CONFIG_LIBDIR empty
+# Prevent use of packages from the host by setting PKG_CONFIG_LIBDIR to the
+# staging dir, assuming no .pc file is present there. Using an empty string
+# does not work with pkgconf.
 TARGET_PKG_CONFIG_ENV := \
 	PKG_CONFIG="$(PKGCONFIG_BIN)" \
 	PKG_CONFIG_PATH="$(TARGET_PKG_CONFIG_PATH)"
@@ -281,7 +283,7 @@ ifeq ("$(TARGET_OS_FLAVOUR)","native")
   TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR=""
 else
   TARGET_PKG_CONFIG_ENV += PKG_CONFIG_SYSROOT_DIR="$(TARGET_OUT_STAGING)"
-  TARGET_PKG_CONFIG_ENV += PKG_CONFIG_LIBDIR=""
+  TARGET_PKG_CONFIG_ENV += PKG_CONFIG_LIBDIR="$(TARGET_OUT_STAGING)"
 endif
 
 # Environment to use when executing configure script
