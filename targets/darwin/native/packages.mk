e0c7a7fe14d04baac36fb42bb1cc849031fc0202
Aurélien Barre <aurelien.barre@parrot.com>
2019-01-08 12:07:51 +0100

    [DEV] macos: add zlib prebuilt package
    Change-Id: I4698df010ddc03328a727a5fda0b2cfc284985b6
    Reviewed-on: https://gerrit-doc.parrot.biz/103542
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- targets/darwin/native/packages.mk
+++ targets/darwin/native/packages.mk
@@ -5,3 +5,10 @@
 ##
 ## Additional packages for darwin/native target.
 ###############################################################################
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := zlib
+LOCAL_EXPORT_LDLIBS := -lz
+include $(BUILD_PREBUILT)
