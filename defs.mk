81757fef2f2161c413e5e0468fff4a414bfc5b00
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2019-08-13 14:42:35 +0200

    [DEV] Remove the depends.build internal variable.
    - Put build-only dependencies in the 'normal' dependency list so it will
      be recursively analyzed.
    
    Change-Id: Ie9f3991bd574b74b6087b7c92d98ca08e11da16a
    Reviewed-on: https://gerrit-doc.parrot.biz/115995
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- defs.mk
+++ defs.mk
@@ -239,7 +239,6 @@ modules-fields-depends := \
 	depends.STATIC_LIBRARIES \
 	depends.WHOLE_STATIC_LIBRARIES \
 	depends.SHARED_LIBRARIES \
-	depends.build \
 	depends.runtime \
 	depends.headers \
 	depends.all
@@ -521,10 +520,10 @@ __module-check-depends = \
 	$(call __module-check-libs-class,$1,STATIC_LIBRARIES,STATIC_LIBRARY) \
 	$(call __module-check-libs-class,$1,SHARED_LIBRARIES,SHARED_LIBRARY)
 
-# Check direct (and build order) dependencies
+# Check direct dependencies
 # $1 : module name.
 __module-check-depends-direct = \
-	$(foreach __lib,$(__modules.$1.depends) $(__modules.$1.build), \
+	$(foreach __lib,$(__modules.$1.depends), \
 		$(if $(call is-module-registered,$(__lib)), \
 			$(if $(call is-module-in-build-config,$(__lib)),$(empty), \
 				$(if $(call is-var-defined,TARGET_TEST),$(empty), \
@@ -766,8 +765,8 @@ __module-compute-depends-direct = \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.META_PACKAGES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.PREBUILT_LIBRARIES)) \
 	$(call __module-add-depends-direct,$1,$(__modules.$1.EXTERNAL_LIBRARIES)) \
+	$(call __module-add-depends-direct,$1,$(__modules.$1.DEPENDS_MODULES)) \
 	$(eval __modules.$1.depends.headers := $(__modules.$1.DEPENDS_HEADERS)) \
-	$(eval __modules.$1.depends.build += $(__modules.$1.DEPENDS_MODULES)) \
 	$(eval __modules.$1.depends.runtime += $(__modules.$1.REQUIRED_MODULES))
 
 # Add direct dependencies to a module
@@ -928,15 +927,15 @@ module-get-listed-autoconf = $(strip \
 module-get-static-depends = \
 	$(__modules.$1.depends.$2)
 
-# Get all dependencies (except the ones required only for build order)
+# Get all dependencies (all stuff required to be present AND built before)
 module-get-all-depends = \
 	$(__modules.$1.depends.all)
 
-# Get all build dependencies (including the ones required only for build order)
+# Get all build dependencies (stuff required to be present but not necessarily
+# built first)
 module-get-build-depends = \
 	$(__modules.$1.depends.all) \
 	$(__modules.$1.depends.headers) \
-	$(__modules.$1.depends.build) \
 	$(filter $(TARGET_GLOBAL_PREREQUISITES),$(__modules))
 
 # Get headers dependencies
@@ -950,7 +949,6 @@ module-get-depends = \
 # Get dependencies for configuration
 module-get-config-depends = \
 	$(__modules.$1.depends) \
-	$(__modules.$1.depends.build) \
 	$(__modules.$1.depends.runtime)
 
 ###############################################################################
