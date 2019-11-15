2f26b21071f5f9764e57eba5c6f43292de05673b
M. Domenzain <luis.domenzain@parrot.com>
2019-11-15 14:01:22 +0100

    [DEBUG] fix executed detection in zsh
    The current implementation results in /bin/ being added to the PATH when sourced
    in zsh, making the entire wrapper useless when sourced.
    
    This relies on the executing shell being /bin/sh, guaranteed by the shebang #!
    zsh returns the sourced script's basename instead of the shell's.
    
    Change-Id: I31b788329f8e815ac01edc08298ef5c2d55fc05a
    Reviewed-on: https://gerrit-doc.parrot.biz/121754
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- scripts/native-wrapper.sh
+++ scripts/native-wrapper.sh
@@ -11,15 +11,15 @@
 
 # Get full path to this script (either when executed or sourced)
 executed=0
-if [ "$(basename -- $0)" = "native-wrapper.sh" ]; then
-	SCRIPT_PATH=$(cd $(dirname -- $0) >/dev/null && pwd -P)
-	executed=1
-elif [ -n "${BASH_SOURCE}" ]; then
+if [ -n "${BASH_SOURCE}" ]; then
 	# Sourced by bash
 	SCRIPT_PATH=$(cd $(dirname -- ${BASH_SOURCE}) >/dev/null && pwd -P)
 elif [ -n "${ZSH_VERSION}" ]; then
 	# Sourced by zsh
 	SCRIPT_PATH=$(cd $(dirname -- ${(%):-%N}) >/dev/null && pwd -P)
+elif [ "$(basename -- $0)" = "native-wrapper.sh" ]; then
+	    SCRIPT_PATH=$(cd $(dirname -- $0) >/dev/null && pwd -P)
+	    executed=1
 else
 	echo "Unsupported shell"
 fi
