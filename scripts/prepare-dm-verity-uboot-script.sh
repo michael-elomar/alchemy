5f9f0d89a1f83bfba334d778013f2506874f3417
Matthieu CASTET <matthieu.castet@parrot.com>
2020-05-07 10:56:25 +0200

    [DEV SWH-440] allow to generate verity ext4 image
    an improvement could be to extract and modify the size from
    TARGET_IMAGE_OPTIONS to reserve and pad the image.
    For now we expect that the size of ext4 is smaller than the partition.
    
    Change-Id: I8af87384164796deb346230a6fe6eb9c8b053b59
    Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/134068
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Mathieu Le mauff <mathieu.lemauff.ext@parrot.com>
    Reviewed-by: Chidambar Zinnoury <chidambar.zinnoury.ext@parrot.com>
    Reviewed-by: Thierry Baldo <thierry.baldo@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

---
+++ scripts/prepare-dm-verity-uboot-script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+while read line; do
+	key=$(echo ${line} | cut -f1 -d':')
+	value=$(echo ${line} | cut -f2 -d':')
+
+	case "${key}" in
+	"UUID")
+		UUID=${value}
+		;;
+	"Data blocks")
+		DATA_BLOCKS=${value}
+		;;
+	"Data block size")
+		DATA_BLOCK_SIZE=${value}
+		;;
+	"Hash block size")
+		HASH_BLOCK_SIZE=${value}
+		;;
+	"Hash algorithm")
+		HASH_ALG=${value}
+		;;
+	"Salt")
+		SALT=${value}
+		;;
+	"Root hash")
+		ROOT_HASH=${value}
+		;;
+	esac
+done
+
+echo setenv verity_sectors $((${DATA_BLOCKS} * ${DATA_BLOCK_SIZE} / 512))
+echo setenv verity_data_blocks ${DATA_BLOCKS}
+echo setenv verity_hash_start $((${DATA_BLOCKS} * ${DATA_BLOCK_SIZE} / ${HASH_BLOCK_SIZE} + 1))
+echo setenv verity_data_block_sz ${DATA_BLOCK_SIZE}
+echo setenv verity_hash_block_sz ${HASH_BLOCK_SIZE}
+echo setenv verity_hash_alg ${HASH_ALG}
+echo setenv verity_salt ${SALT}
+echo setenv verity_root_hash ${ROOT_HASH}
+echo setenv verity_extra 
