786ea7a489f66457bd6d643651e472097343cdd6
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2020-10-27 18:53:20 +0100

    [DEV] Do not generate doxygen doc for python files
    Change-Id: Icbd356ca3ce023255202cf59ee660437df8d8db0
    Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/147015
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- classes/extra-rules.mk
+++ classes/extra-rules.mk
@@ -44,7 +44,7 @@ $(LOCAL_MODULE)-doc:
 		echo "PROJECT_BRIEF=\"$(PRIVATE_DESCRIPTION)\""; \
 		echo "GENERATE_XML=YES"; \
 		echo "INPUT=$(PRIVATE_INPUT)"; \
-		echo "EXCLUDE_PATTERNS+=.git out sdk"; \
+		echo "EXCLUDE_PATTERNS+=.git out sdk *.py"; \
 		echo "OUTPUT_DIRECTORY=$(PRIVATE_DOC_DIR)"; \
 	) | doxygen - &> $(PRIVATE_DOC_DIR)/doxygen.log
 else
@@ -78,7 +78,7 @@ $(LOCAL_MODULE)-doc:
 		echo "WARN_IF_DOC_ERROR=NO"; \
 		echo "RECURSIVE=YES"; \
 		echo "INPUT=$(PRIVATE_INPUT)"; \
-		echo "EXCLUDE_PATTERNS+=.git out sdk"; \
+		echo "EXCLUDE_PATTERNS+=.git out sdk *.py"; \
 		echo "OUTPUT_DIRECTORY=$(PRIVATE_DOC_DIR)"; \
 	) | doxygen - &> $(PRIVATE_DOC_DIR)/doxygen.log
 
