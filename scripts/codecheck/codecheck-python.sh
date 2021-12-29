84cfe39427851193b5d10ae08dd48328f64f1222
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2021-12-29 18:02:14 +0100

    [DEV] Update flake8 output to match other codechecks.
    Change-Id: I41b6457a87a0b4e7fb5c4faf7830fb57e9b2afc1
    Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/178965
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- scripts/codecheck/codecheck-python.sh
+++ scripts/codecheck/codecheck-python.sh
@@ -11,7 +11,7 @@ for CHECKER in ${CHECKERS}; do
 	if [ "${CHECKER}" = "pep8" ]; then
 		python3 -m pycodestyle ${ARGS} ${FILES}
 	elif [ "${CHECKER}" = "flake8" ]; then
-		flake8 ${ARGS} ${FILES}
+		flake8 --format="%(path)s:%(row)d: WARNING:%(code)s: %(text)s" ${ARGS} ${FILES}
 	else
 		echo "Unknown 'python' checker '${CHECKER}'"
 	fi
