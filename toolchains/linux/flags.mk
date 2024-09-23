6c9f4a1a48664d4a4a41ca23625b8872659cf096
Matthias Rabault <matthias.rabault@parrot.com>
2024-09-23 11:17:21 +0200

    [DEV] Make sure no one tries to -I/usr/include
    This has already happened, see
    https://gerrit-doc.parrot.biz/c/os/os_packages/debug/+/246177.
    
    Change-Id: Ie2afb2ad7a75c8e85d2207445e24195b171636b9
    Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/246283
    Reviewed-by: Matthias Rabault <matthias.rabault@parrot.com>
    Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>
    Reviewed-by: Arthur Dauzat <arthur.dauzat@parrot.com>

--- toolchains/linux/flags.mk
+++ toolchains/linux/flags.mk
@@ -9,3 +9,12 @@
 ifneq ("$(TARGET_LIBC)","")
   -include $(BUILD_SYSTEM)/toolchains/$(TARGET_OS)/$(TARGET_LIBC)/flags.mk
 endif
+
+ifeq ("$(TARGET_OS_FLAVOUR)-$(TARGET_CC_FLAVOUR)","yocto-gcc")
+  # This is not part of regular GCC, but Yocto builds of GCC support this to
+  # prevent broken projects from adding things like -I/usr/include, which doesn't
+  # make sense in our case.
+  # Qualcomm's Clang toolchain seems to ignore this warning. but regular Clang
+  # toolchains throw an error because this option is not recognized.
+  TARGET_GLOBAL_CFLAGS += -Werror=poison-system-directories
+endif
