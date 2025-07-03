michael-elomar/alchemy

commit d4332f3ce1d5a6a51068740bfcb878fd1f02fc67

[BUILD] cmake: use host module binary if host.cmake is available
Change-Id: Ic59fed40efa43c46b7ed00531987062efcef5a96
Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/256176
Reviewed-by: Nicolas Fugier <nicolas.fugier@parrot.com>
Tested-by: Jenkins_dragon <jenkins.dragon@parrot.com>

classes/CMAKE/register.mk
@@ -15,5 +15,8 @@ LOCAL_MODULE_CLASS := CMAKE
 LOCAL_MODULE_FILENAME := $(LOCAL_MODULE).done
 LOCAL_DONE_FILES += $(LOCAL_MODULE).done
 
+# Add optional host.cmake dependency
+LOCAL_CONDITIONAL_LIBRARIES += OPTIONAL:host.cmake
+
 # Register in the system
 $(module-add)

classes/CMAKE/setup.mk
@@ -45,12 +45,20 @@ define _cmake-def-cmd-clean
 	fi;
 endef
 
+# Update host compilation path
+_cmake_host_path := $(call make-path-list, \
+        $(HOST_OUT_STAGING)/bin \
+        $(HOST_OUT_STAGING)/$(HOST_DEFAULT_BIN_DESTDIR) \
+        $(call split-path-list,$(PATH)) \
+)
+
 ###############################################################################
 ## Variables used for cmake.
 ###############################################################################
 
+
 ifndef CMAKE
-  CMAKE := $(shell which cmake 2>/dev/null)
+  CMAKE := $(shell PATH="$(_cmake_host_path)" which cmake 2>/dev/null)
 endif
 
 ifeq ("$(TARGET_OS)","linux")

commit 9f40a4a731949a7ba2e3dfe1b02a4ce48204f792

[DEV] Fix dependency bug for CMake packages
Bug introduced by d4332f3ce1d5a6a51068740bfcb878fd1f02fc67, seen during
units tests using Jenkins.

The original patch causes host.cmake to end up in __modules.$1.depends.all
(but not __modules.$1.depends).
Since the target is not a host module, this triggers a safeguard that
prevents Alchemy users from making an aarch64 package use a x86_64
library.

Change-Id: I42d762175536a81b8e498be4be4d44c6180fad4b
Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/261957
Reviewed-by: Hugo Sarbia <hugo.sarbia@parrot.com>
Reviewed-by: Matthias Rabault <matthias.rabault@parrot.com>

classes/CMAKE/register.mk
@@ -15,8 +15,10 @@ LOCAL_MODULE_CLASS := CMAKE
 LOCAL_MODULE_FILENAME := $(LOCAL_MODULE).done
 LOCAL_DONE_FILES += $(LOCAL_MODULE).done
 
-# Add optional host.cmake dependency
-LOCAL_CONDITIONAL_LIBRARIES += OPTIONAL:host.cmake
+# If host.cmake is enabled, we're going to use it, hence
+# LOCAL_DEPENDS_HOST_MODULES for proper ordering
+$(if $(call is-module-in-build-config,host.cmake), \
+  $(eval LOCAL_DEPENDS_HOST_MODULES += host.cmake),)
 
 # Register in the system
 $(module-add)