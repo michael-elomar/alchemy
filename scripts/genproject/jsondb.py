4e2d419fe65bf21d9fa5d5be358a36264459b00e
Thomas Poussevin <thomas.poussevin@parrot.com>
2025-12-03 14:59:40 +0100

    [DEBUG] genjsondb: fix -I and -isystem mistakes
    * jsondb.py: don't add -I if -isystem is alerady present
    
    The problem appears on building -t genjsondb.
    generated command contains "-I-isystem", so headers are not found,
    and it raise fake alerts due to missing prototypes
    
    Change-Id: If58b8a51862dae91f5276614f2c4a6909c4562ba
    Reviewed-on: https://gerrit-doc.parrot.biz/c/build/Alchemy/+/270819
    Reviewed-by: Hugo Sarbia <hugo.sarbia@parrot.com>
    Reviewed-by: Matthias Rabault <matthias.rabault@parrot.com>

--- scripts/genproject/jsondb.py
+++ scripts/genproject/jsondb.py
@@ -12,7 +12,10 @@ C_EXTENSIONS = ['.c', '.C']
 def _build_base_command_c(project):
     base_command_c = [project.get_target_var("CC"), "-c"]
     for include in project.includes:
-        base_command_c.append("-I" + include)
+        if include.startswith("-"):
+            base_command_c.append(include)
+        else:
+            base_command_c.append("-I" + include)
     for autoconf_h_file in project.autoconf_h_files:
         base_command_c.append("-include " + autoconf_h_file)
     for define, value in project.defines_c.items():
@@ -27,7 +30,10 @@ def _build_base_command_c(project):
 def _build_base_command_cxx(project):
     base_command_cxx = [project.get_target_var("CXX"), "-c"]
     for include in project.includes:
-        base_command_cxx.append("-I" + include)
+        if include.startswith("-"):
+            base_command_cxx.append(include)
+        else:
+            base_command_cxx.append("-I" + include)
     for autoconf_h_file in project.autoconf_h_files:
         base_command_cxx.append("-include " + autoconf_h_file)
     defines = {}
